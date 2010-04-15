7c2d1d0f3a45b2d4919b9b8d73db096939c73cf9
Clayton Sims <csims@dimagi.com>
2010-04-15 14:30:34 -0400

    A simple localization change.

--- application/src/org/commcare/view/CommCareSuiteView.java
+++ application/src/org/commcare/view/CommCareSuiteView.java
@@ -5,6 +5,8 @@ package org.commcare.view;
 
 import javax.microedition.lcdui.Command;
 
+import org.javarosa.core.services.locale.Localization;
+
 import de.enough.polish.ui.List;
 import de.enough.polish.ui.TextField;
 
@@ -13,7 +15,7 @@ import de.enough.polish.ui.TextField;
  *
  */
 public class CommCareSuiteView extends List{	
-	public final static Command BACK = new Command("Back", Command.BACK, 0);
+	public final static Command BACK = new Command(Localization.get("command.back"), Command.BACK, 0);
 	
 	public CommCareSuiteView(String title) {
 		super(title, List.IMPLICIT);
