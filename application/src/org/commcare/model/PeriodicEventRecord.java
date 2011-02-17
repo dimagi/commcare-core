67b0848d69473e3ca9f02a3c87c66fcbcb26160c
Clayton Sims <csims@dimagi.com>
2011-02-17 13:45:31 -0500

    more docs

--- application/src/org/commcare/model/PeriodicEventRecord.java
+++ application/src/org/commcare/model/PeriodicEventRecord.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.commcare.model;
 
 import java.io.DataInputStream;
@@ -16,6 +13,14 @@ import org.javarosa.core.util.externalizable.ExtUtil;
 import org.javarosa.core.util.externalizable.PrototypeFactory;
 
 /**
+ * A periodic event record is a database model for maintaining the
+ * execution scheduling of periodic events. There should be one or 
+ * zero records for each type of periodic event, maintaining a record
+ * of the next and previous occurrences of that event.
+ * 
+ * TODO: Add general functionality for maintaining other state for
+ * these records: Disabled, etc. 
+ * 
  * @author ctsims
  *
  */
@@ -23,6 +28,7 @@ public class PeriodicEventRecord implements Persistable, IMetaData {
 	
 	public static final String STORAGE_KEY = "periodic_e_r";
 	
+	/** The type of event that this record is maintaining */
 	public static final String META_EVENT_KEY = "event_key";
 	
 	private int recordId = -1;
@@ -34,12 +40,21 @@ public class PeriodicEventRecord implements Persistable, IMetaData {
 	private String key;
 	
 	/**
-	 * DO NOT CALL. FOR SERIALIZATION ONLY
+	 * DO NOT CALL. FOR SERIALIZATION ONLY.
+	 * 
+	 * Records will be created by the scheduler. 
 	 */
 	public PeriodicEventRecord() {
 		//FOR SERIALIZATION ONLY!!!
 	}
 	
+	/**
+	 * Creates a record for a periodic event. Should only be called by the
+	 * package. 
+	 * 
+	 * @param key
+	 * @param nextOccurance
+	 */
 	protected PeriodicEventRecord(String key, Date nextOccurance) {
 		this.key = key;
 		this.lastOccurance = new Date(0);
@@ -82,6 +97,10 @@ public class PeriodicEventRecord implements Persistable, IMetaData {
 		ExtUtil.write(out, key);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see org.javarosa.core.services.storage.IMetaData#getMetaData()
+	 */
 	public Hashtable getMetaData() {
 		Hashtable ret = new Hashtable();
 		
@@ -90,11 +109,19 @@ public class PeriodicEventRecord implements Persistable, IMetaData {
 		}
 		return ret;
 	}
-
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.javarosa.core.services.storage.IMetaData#getMetaData()
+	 */
 	public String[] getMetaDataFields() {
 		return new String[] {META_EVENT_KEY};
 	}
-
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.javarosa.core.services.storage.IMetaData#getMetaData()
+	 */
 	public Object getMetaData(String fieldName) {
 		if(META_EVENT_KEY.equals(fieldName)) {
 			return key;
@@ -103,22 +130,37 @@ public class PeriodicEventRecord implements Persistable, IMetaData {
 		}
 	}
 
+	/**
+	 * @return The next scheduled time for this event to occur.
+	 */
 	public Date getNextTrigger() {
 		return nextOccurance;
 	}
 
+	/**
+	 * @return The unique event key tied to this record.
+	 */
 	public String getEventKey() {
 		return key;
 	}
 
+	/**
+	 * @param date The time this event last occurred.
+	 */
 	public void setLastOccurance(Date date) {
 		this.lastOccurance = date;
 	}
 
+	/**
+	 * @param nextSchedule The next time this event should be triggered.
+	 */
 	public void setNextScheduledDate(Date nextSchedule) {
 		this.nextOccurance = nextSchedule;
 	}
 
+	/**
+	 * @return The last time this event occurred
+	 */
 	public Date getLastOccurance() {
 		return lastOccurance;
 	}
