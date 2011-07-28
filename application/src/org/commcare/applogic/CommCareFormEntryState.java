f79e23cf8aa2bb4137229ace8804838eeca7cb24
clayton sims <csims@dimagi.com>
2011-07-28 17:12:55 -0400

    Enabled SMS and <submission> based form transmit outcomes. SMS messages can't get cached and have very little feedback at this point, though

--- application/src/org/commcare/applogic/CommCareFormEntryState.java
+++ application/src/org/commcare/applogic/CommCareFormEntryState.java
@@ -13,6 +13,8 @@ import org.commcare.util.CommCareUtil;
 import org.commcare.util.FormTransportWorkflow;
 import org.javarosa.cases.util.CaseModelProcessor;
 import org.javarosa.core.model.FormDef;
+import org.javarosa.core.model.SubmissionProfile;
+import org.javarosa.core.model.condition.EvaluationContext;
 import org.javarosa.core.model.condition.IFunctionHandler;
 import org.javarosa.core.model.instance.FormInstance;
 import org.javarosa.core.model.util.restorable.RestoreUtils;
@@ -32,14 +34,18 @@ import org.javarosa.formmanager.view.IFormEntryView;
 import org.javarosa.formmanager.view.chatterbox.Chatterbox;
 import org.javarosa.formmanager.view.singlequestionscreen.SingleQuestionView;
 import org.javarosa.j2me.view.J2MEDisplay;
-import org.javarosa.model.xform.XFormSerializingVisitor;
+import org.javarosa.model.xform.XPathReference;
+import org.javarosa.services.transport.SubmissionTransportHelper;
 import org.javarosa.services.transport.TransportMessage;
 import org.javarosa.services.transport.TransportService;
 import org.javarosa.services.transport.impl.TransportException;
+import org.javarosa.xpath.XPathParseTool;
 
 //can't support editing saved forms; for new forms only
 public abstract class CommCareFormEntryState extends FormEntryState {
-
+	
+	public static final String METHOD_SMS = "smspush";
+	public static final String METHOD_POST = "post";
 	private String formName;
 	private Vector<IPreloadHandler> preloaders;
 	private Vector<IFunctionHandler> funcHandlers;
@@ -89,15 +95,15 @@ public abstract class CommCareFormEntryState extends FormEntryState {
 			// This process will take place in three parts, in order to ensure that the server and client don't get
 			// out of sync. 
 			
+			
 			// 1) First we'll ensure that a transport message or other off-line element can be created and
-			// saved. This should establish that it will be sent from the device no matter what.
+			// saved. This should establish that it will be sent from the device no matter what.		
 			
-			
-			//Figure out what the 'send' action is
-			String action = PropertyManager._().getSingularProperty(CommCareProperties.SEND_STYLE);
+			//First, figure out what the submission action is
+			SubmissionProfile profile = CommCareFormEntryState.getSubmissionProfile(form, instanceData);
 			
 			// Get the workflow that will be responsible for managing the flow of form sending
-			FormTransportWorkflow workflow = getWorkflowFactory(action);
+			FormTransportWorkflow workflow = getWorkflowFactory(profile);
 			
 			//Let the workflow perform its first action
 			workflow.preProcessing(instanceData);
@@ -123,6 +129,38 @@ public abstract class CommCareFormEntryState extends FormEntryState {
 		} else {
 			abort();
 		}
+	}
+	
+	protected static SubmissionProfile getSubmissionProfile(FormDef form, FormInstance instanceData) {
+		
+		// See if the form wants to force a specific submission type
+		SubmissionProfile profile = form.getSubmissionProfile();
+		
+		//TODO: Are there any circumstances in which the phone's send type should override the phone's options?
+		if(profile == null) {
+			String style = PropertyManager._().getSingularProperty(CommCareProperties.SEND_STYLE);
+			if(CommCareProperties.SEND_STYLE_HTTP.equals(style)) {
+				//CAN'T BE SERIALIZED
+				profile = new SubmissionProfile(new XPathReference("/"),METHOD_POST,CommCareContext._().getSubmitURL(),null);
+			} else {
+				//No idea
+				profile = new SubmissionProfile(new XPathReference("/"),PropertyManager._().getSingularProperty(CommCareProperties.SEND_STYLE),null,null);
+			}
+
+		}
+		
+		//See if the action is actually a reference into the form.
+		String action = profile.getAction();
+		try {
+			String newAction = (String)XPathParseTool.parseXPath("string(" + action + ")").eval(instanceData, new EvaluationContext(form.exprEvalContext, instanceData.getRoot().getRef()));
+			
+			profile = new SubmissionProfile(profile.getRef(), profile.getMethod(), newAction, profile.getMediaType());
+		} catch(Exception e) {
+			//Expected use case for most static #'s.
+			e.printStackTrace();
+		}
+		
+		return profile;
 	}
 
 	public void suspendForMediaCapture(int captureType) {
@@ -173,8 +211,8 @@ public abstract class CommCareFormEntryState extends FormEntryState {
 	 *  TODO: This can/should be shared with JavaRosa core, probably. Safest way to stay in sync
 	 *  WRT SMS transport, etc.
 	 */
-	private FormTransportWorkflow getWorkflowFactory(String action) {
-		if(CommCareProperties.SEND_STYLE_NONE.equals(action)) {
+	private FormTransportWorkflow getWorkflowFactory(final SubmissionProfile profile) {
+		if(CommCareProperties.SEND_STYLE_NONE.equals(profile.getMethod())) {
 			return new FormTransportWorkflow() {
 				
 				public void preProcessing(FormInstance instance) {
@@ -184,7 +222,7 @@ public abstract class CommCareFormEntryState extends FormEntryState {
 					CommCareFormEntryState.this.goHome();
 				}
 			};
-		}else if(CommCareProperties.SEND_STYLE_FILE.equals(action)) {
+		} else if(CommCareProperties.SEND_STYLE_FILE.equals(profile.getMethod())) {
 			return new FormTransportWorkflow() {
 				public void preProcessing(FormInstance instance) {
 					// TODO: Save data to file system
@@ -197,14 +235,24 @@ public abstract class CommCareFormEntryState extends FormEntryState {
 		} else {
 			return new FormTransportWorkflow() {
 				TransportMessage message;
+				boolean cacheable;
 				
 				public void preProcessing(FormInstance instance) {
 					//HTTP or Fallback(Nothing)
 					//This actually won't send anything just yet. It'll just put it straight in the cache
 					try {
-						message = CommCareContext._().buildMessage(new XFormSerializingVisitor().createSerializedPayload(instance));
-						TransportService.send(message, 0, 0);
-						Logger.log("formentry", "create txmsg " + PropertyUtils.trim(message.getCacheIdentifier(), 6));
+						cacheable = profile.getMethod().equals(METHOD_POST);
+						message = SubmissionTransportHelper.createMessage(instance, profile, cacheable);
+						
+						if(cacheable) {
+							//cache it
+							TransportService.send(message, 0, 0);
+							Logger.log("formentry", "create txmsg " + PropertyUtils.trim(message.getCacheIdentifier(), 6));
+						} else{
+							Logger.log("formentry", "created message of type " + profile.getMethod() + ", and autosending");
+							//send it
+							TransportService.send(message, 3, 0);
+						}
 						
 						// If there is a failure building or caching the transport message, we don't want to further process data.
 						// Otherwise the phone will have data that can never get to the server.
@@ -219,7 +267,7 @@ public abstract class CommCareFormEntryState extends FormEntryState {
 					} 
 				}
 				public void postProcessing() {
-					CommCarePostFormEntryState httpAskSendState = new CommCarePostFormEntryState(message, CommCareSense.isAutoSendEnabled()) {
+					CommCarePostFormEntryState httpAskSendState = new CommCarePostFormEntryState(message, CommCareSense.isAutoSendEnabled() || cacheable) {
 						public void goHome() {
 							//If we're autosending, make sure to expire old deadlines
 							if(CommCareSense.isAutoSendEnabled()) {
