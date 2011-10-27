35bb3d17cf47e5e2afd7de020d23b12bdabbe015
clayton sims <csims@dimagi.com>
2011-10-27 15:20:23 -0400

    Fixed an inconsitency in case processing that didn't permit overwriting values as was possible when pulling from a server (or processing on ODK)

--- j2me/objects/org.javarosa.cases/src/org/javarosa/cases/util/CaseModelProcessor.java
+++ j2me/objects/org.javarosa.cases/src/org/javarosa/cases/util/CaseModelProcessor.java
@@ -206,10 +206,14 @@ public class CaseModelProcessor implements ICaseModelProcessor {
 			} else{
 				String vname = kid.getName();
 				
+				//We skip nodes which aren't relevant above by completely ignoring them. If a node has a null value, that means
+				//that it exists and is simply empty, so we need to set a valid property vlaue for it (empty string), so that 
+				//properties can be overriden.
+				String value = "";
 				if(kid.getValue() != null) {
-					String value = kid.getValue().uncast().getString();
-					c.setProperty(vname, value);
+					value = kid.getValue().uncast().getString();
 				}
+				c.setProperty(vname, value);
 			}
 		}
 		commit(c);
