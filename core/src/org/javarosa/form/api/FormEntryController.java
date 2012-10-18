8687f19d7f8a8457fd9e331bd2d29efa1d7ab758
wspride@wspride-PC <wspride@wspride-PC>
2012-10-18 12:04:53 -0400

    code cleanup

--- core/src/org/javarosa/form/api/FormEntryController.java
+++ core/src/org/javarosa/form/api/FormEntryController.java
@@ -87,7 +87,6 @@ public class FormEntryController {
         if (element.isRequired() && data == null) {
             return ANSWER_REQUIRED_BUT_EMPTY;
         } else if (!complexQuestion && !model.getForm().evaluateConstraint(index.getReference(), data)) {
-        	System.out.println(":::violated entered");
             return ANSWER_CONSTRAINT_VIOLATED;
         } else if (!complexQuestion) {
             commitAnswer(element, index, data);
