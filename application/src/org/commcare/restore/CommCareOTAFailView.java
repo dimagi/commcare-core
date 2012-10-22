77e0b613471e653cabf0e7fdfc5bea8b5e8b04bb
wspride@wspride-PC <wspride@wspride-PC>
2012-10-22 17:27:34 -0400

    missed a class, failview

---
+++ application/src/org/commcare/restore/CommCareOTAFailView.java
@@ -0,0 +1,87 @@
+/**
+ * 
+ */
+package org.commcare.restore;
+
+import javax.microedition.lcdui.Canvas;
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.Gauge;
+
+import org.javarosa.core.services.locale.Localization;
+
+import de.enough.polish.ui.Form;
+import de.enough.polish.ui.StringItem;
+import de.enough.polish.ui.UiAccess;
+
+/**
+ * @author ctsims
+ *
+ */
+public class CommCareOTAFailView extends Form{
+
+	public static final Command DOWNLOAD = new Command(Localization.get("restore.retry"), Command.OK, 1);
+	public static final Command CANCEL = new Command(Localization.get("polish.command.cancel"), Command.BACK, 2);
+	
+	private StringItem fetch;
+	
+	private final static int RESOLUTION = 100;
+	
+	StringItem details;
+	String buffer;
+	
+	StringItem failTitle;
+	String failMessage = "Restore failed for reasons below. Retry?";
+	
+	int count = 0;
+	
+	boolean finished;
+	boolean gaugeIsInfinite = true;
+	int totalItems;
+
+	public CommCareOTAFailView(String title) {
+		super(title);
+		details = new StringItem("","");
+		
+		failTitle = new StringItem(failMessage,"");
+		this.append(failTitle);
+		
+		this.append(details);
+		buffer = "";
+		
+		this.addCommand(DOWNLOAD);
+		this.addCommand(CANCEL);
+	}
+	
+	public void addToMessage(String message) {
+		buffer = message + "\n" + buffer;
+		setMessage(buffer);
+	}
+	
+	public void setMessage(String message) {
+		buffer = message;
+		details.setText(buffer);	
+	}
+	
+	protected boolean handleKeyReleased(int keyCode, int gameAction) {
+		if(super.handleKeyReleased(keyCode, gameAction)) {
+			//Don't do anything that already does something.
+			return true;
+		} else {
+			if(finished) {
+				//scrolling should be uninterrupted
+				switch(gameAction) {
+				case Canvas.UP:
+				case Canvas.DOWN:
+				case Canvas.LEFT:
+				case Canvas.RIGHT:
+					return false;
+				default:
+					System.out.println("entered default case");
+					UiAccess.cast(this.getCommandListener()).commandAction(CANCEL,this);
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
