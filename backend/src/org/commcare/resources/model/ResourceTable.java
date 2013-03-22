fa3dfe341e2531ad833fa91ed1d1596dd4c0fc4e
wspride@wspride-PC <wspride@wspride-PC>
2013-03-22 19:00:55 -0400

    code cleanup

--- backend/src/org/commcare/resources/model/ResourceTable.java
+++ backend/src/org/commcare/resources/model/ResourceTable.java
@@ -32,12 +32,6 @@ public class ResourceTable {
 
 	private IStorageUtilityIndexed storage;
 	private InstallerFactory factory;
-	
-	public final static int RESOURCE_TABLE_EMPTY = 0;
-	public final static int RESOURCE_TABLE_INSTALLED = 1;
-	public final static int RESOURCE_TABLE_PARTIAL = 2;
-	public final static int RESOURCE_TABLE_UPGRADE = 3;
-	
 
 	/**
 	 * For Serialization Only!
@@ -62,34 +56,6 @@ public class ResourceTable {
 		table.storage = storage;
 		table.factory = factory;
 		return table;
-	}
-	
-	/**
-	 * @return status representing table preparedness
-	 * 	RESOURCE_TABLE_EMPTY
-	 * 	RESOURCE_TABLE_INSTALLED
-	 * 	RESOURCE_TABLE_PARTIAL
-	 */
-	public int getTableReadiness(){
-		
-		boolean isFullyInstalled = true;
-		boolean isEmpty = true;
-		
-		for(IStorageIterator it = storage.iterate(); it.hasMore();) {
-			Resource r = (Resource)it.nextRecord();
-
-			if (r.getStatus() != Resource.RESOURCE_STATUS_INSTALLED) {
-				isFullyInstalled = false;
-			}
-			
-			if(r.getStatus() != Resource.RESOURCE_STATUS_UNINITIALIZED){
-				isEmpty = false;
-			}
-		}
-		
-		if(isEmpty){return RESOURCE_TABLE_EMPTY;}
-		if(isFullyInstalled){return RESOURCE_TABLE_INSTALLED;}
-		return RESOURCE_TABLE_PARTIAL;
 	}
 	
 	public String getTableReadinessString() {
