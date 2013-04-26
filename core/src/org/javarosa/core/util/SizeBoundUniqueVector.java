13b45dbfff24a467873319d3f34de21fc2b7056f
wspride@wspride-PC <wspride@wspride-PC>
2013-04-26 06:05:37 -0400

    multimedia refactor

---
+++ core/src/org/javarosa/core/util/SizeBoundUniqueVector.java
@@ -0,0 +1,39 @@
+
+package org.javarosa.core.util;
+
+import java.util.Vector;
+
+/**
+ * 
+ * Only use for J2ME Compatible Vectors
+ * 
+ *  A SizeBoundVector that enforces that all member items be unique. You must
+ *  implement the .equals() method of class E
+ * 
+ * @author wspride
+ *
+ */
+public class SizeBoundUniqueVector<E> extends SizeBoundVector<E> {
+	
+	public SizeBoundUniqueVector(int sizeLimit) {
+		super(sizeLimit);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.Vector#addElement(java.lang.Object)
+	 */
+	public synchronized void addElement(E obj) {
+		
+		if(this.size() == limit) {
+			additional++;
+			return;
+		} 
+		else if(this.contains(obj)){
+			return;
+		}
+		else {
+			super.addElement(obj);
+		}
+	}
+
+}
