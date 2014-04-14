b6dcb2019322c514d17a85a4e153e5dc14004e73
wspride@wspride-PC.ipv6.ihub.co.ke <wspride@wspride-PC.ipv6.ihub.co.ke>
2014-04-14 18:30:35 -0400

    add audio checks before playing

--- core/src/org/javarosa/core/util/PropertyUtils.java
+++ core/src/org/javarosa/core/util/PropertyUtils.java
@@ -18,7 +18,6 @@ package org.javarosa.core.util;
 
 import java.util.Random;
 import java.util.Vector;
-
 import org.javarosa.core.services.Logger;
 import org.javarosa.core.services.PropertyManager;
 
@@ -74,6 +73,11 @@ public class PropertyUtils {
 			PropertyManager._().setProperty("DeviceID", newId);
 			Logger.log("device", "DeviceID set: [" + newId + "]");
 		}
+	}
+	
+	public static boolean playFormAudio(){
+		String playAudio = PropertyManager._().getSingularProperty("cc-play-audio");
+		return (!"No".equals(playAudio));
 	}
 	
 	
