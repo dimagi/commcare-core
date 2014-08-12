7460af78ca975bb0b2a5f46dab792a1ad3a5210c
Jenny Schweers <jschweers@dimagi.com>
2014-08-12 17:36:26 -0400

    Check against xforms, not document namespace

--- core/src/org/javarosa/xform/parse/XFormParser.java
+++ core/src/org/javarosa/xform/parse/XFormParser.java
@@ -105,6 +105,7 @@ public class XFormParser {
 	
 	public static final String NAMESPACE_JAVAROSA = "http://openrosa.org/javarosa";
 	public static final String NAMESPACE_HTML = "http://www.w3.org/1999/xhtml";
+	public static final String NAMESPACE_XFORMS = "http://www.w3.org/2002/xforms";
 
 	private static final int CONTAINER_GROUP = 1;
 	private static final int CONTAINER_REPEAT = 2;
@@ -571,7 +572,7 @@ public class XFormParser {
 				delayedParseElements.addElement(child);
 			} else { //invalid model content
 				if (type == Node.ELEMENT) {
-					if (child.getNamespace().equals(defaultNamespace)) {
+					if (child.getNamespace().equals(NAMESPACE_XFORMS)) {
 						throw new XFormParseException("Unrecognized top-level tag [" + childName + "] found within <model>",child);
 					}
 				} else if (type == Node.TEXT && getXMLText(e, i, true).length() != 0) {
