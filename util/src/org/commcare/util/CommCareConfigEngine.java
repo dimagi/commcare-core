13bacba83713547f7903744b76e2a845d407ee4d
ctsims <csims@dimagi.com>
2014-09-03 14:21:05 -0400

    Update CommCareConfigEngine.java

--- util/src/org/commcare/util/CommCareConfigEngine.java
+++ util/src/org/commcare/util/CommCareConfigEngine.java
@@ -74,7 +74,7 @@ public class CommCareConfigEngine {
             }
 
             public int getMinorVersion() {
-                return 16;
+                return 15;
             }
         };
         
