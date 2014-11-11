6d75a757397aaab81486576c5ab18a8416f1fe0a
Jenny Schweers <jschweers@dimagi.com>
2014-11-11 08:31:12 -0500

    Missed adding a file

---
+++ backend/src/org/commcare/suite/model/graph/TimePointData.java
@@ -0,0 +1,17 @@
+package org.commcare.suite.model.graph;
+
+import java.util.Date;
+
+public class TimePointData extends XYPointData {
+    private Date mTime;
+
+    public TimePointData(Date time, Double y) {
+        super(null, y);
+        mTime = time;
+    }
+    
+    public Date getTime() {
+        return mTime;
+    }
+    
+}
