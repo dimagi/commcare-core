268cdf90eeacffe8734535b1dede61d1c4e7a80f
Clayton Sims <csims@dimagi.com>
2015-01-01 11:15:04 -0500

    Updated DAG Datatype with Edge data and some helper methods

--- core/src/org/javarosa/core/util/DAG.java
+++ core/src/org/javarosa/core/util/DAG.java
@@ -11,24 +11,25 @@ import java.util.Vector;
 /**
  * Directed A-cyclic (NOT ENFORCED) graph datatype.
  * 
- * Genericized with two types: An unique index value (representing the node) and a generic
- * set of data to associate with that node
+ * Genericized with three types: An unique index value (representing the node), a generic
+ * set of data to associate with that node, and a piece of data to associate with each
+ * edge
  * 
  * @author ctsims
  *
  */
-public class DAG<I,N> {
+public class DAG<I,N,E> {
     //TODO: This is a really unsafe datatype. Needs an absurd amount of updating for representation
     //invariance, synchronicity, cycle detection, etc.
     
     Hashtable<I, N> nodes; 
-    Hashtable<I,Vector<I>> edge;
-    Hashtable<I,Vector<I>> inverse;
+    Hashtable<I,Vector<Edge<I, E>>> edge;
+    Hashtable<I,Vector<Edge<I, E>>> inverse;
     
     public DAG() {
         nodes = new Hashtable<I,N>();
-        edge = new Hashtable<I,Vector<I>>();
-        inverse = new Hashtable<I,Vector<I>>();
+        edge = new Hashtable<I,Vector<Edge<I, E>>>();
+        inverse = new Hashtable<I,Vector<Edge<I, E>>>();
     }
     
     public void addNode(I i, N n) {
@@ -40,33 +41,33 @@ public class DAG<I,N> {
      * @param source
      * @param destination
      */
-    public void setEdge(I source, I destination) {
-        addToEdge(edge, source, destination);
-        addToEdge(inverse, destination, source);
+    public void setEdge(I source, I destination, E edgeData) {
+        addToEdge(edge, source, destination, edgeData);
+        addToEdge(inverse, destination, source, edgeData);
     }
     
-    private void addToEdge(Hashtable<I,Vector<I>> edgeList, I a, I b) {
-        Vector<I> edge;
+    private void addToEdge(Hashtable<I,Vector<Edge<I, E>>> edgeList, I a, I b, E edgeData) {
+        Vector<Edge<I, E>> edge;
         if(edgeList.containsKey(a)) {
             edge = edgeList.get(a);
         } else {
-            edge = new Vector<I>();
+            edge = new Vector<Edge<I, E>>();
         }
-        edge.addElement(b);
+        edge.addElement(new Edge<I, E>(b, edgeData));
         edgeList.put(a, edge);
     }
     
-    public Vector<I> getParents(I index) {
+    public Vector<Edge<I,E>> getParents(I index) {
         if(inverse.containsKey(index)) {
             return inverse.get(index);
         } else {
-            return null;
+            return new Vector();
         }
     }
     
-    public Vector<I> getChildren(I index) {
+    public Vector<Edge<I,E>> getChildren(I index) {
         if(!edge.containsKey(index)) {
-            return null;
+            return new Vector();
         } else {
             return edge.get(index);
         }
@@ -82,13 +83,41 @@ public class DAG<I,N> {
      * any edges in the graph
      */
     public Stack<I> getSources() {
-        Stack<I> roots = new Stack();
+        Stack<I> sources = new Stack();
         for(Enumeration en = nodes.keys(); en.hasMoreElements() ; ) {
             I i = (I)en.nextElement();
             if(!inverse.containsKey(i)) {
+                sources.addElement(i);
+            }
+        }
+        return sources;
+    }
+    
+    //Is that the right name?
+    /**
+     * @return Indices for all nodes that do not have any outgoing edges
+     */
+    public Stack<I> getSinks() {
+        Stack<I> roots = new Stack();
+        for(Enumeration en = nodes.keys(); en.hasMoreElements() ; ) {
+            I i = (I)en.nextElement();
+            if(!edge.containsKey(i)) {
                 roots.addElement(i);
             }
         }
         return roots;
+    }
+    
+    public static class Edge<I, E> {
+        public I i ; 
+        public E e;
+        public Edge(I i, E e) {
+            this.i = i;
+            this.e = e;
+        }
+    }
+
+    public Enumeration<N> getNodes() {
+        return nodes.elements();
     }
 }
