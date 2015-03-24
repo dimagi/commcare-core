a5b97757b2d208bce620b73d8bc21947456d5604
Will Pride <wpride@dimagi.om>
2015-03-24 14:30:28 -0600

    add tests

--- core/test/org/javarosa/core/util/test/LocalizationTests.java
+++ core/test/org/javarosa/core/util/test/LocalizationTests.java
@@ -92,7 +92,6 @@ public class LocalizationTests extends TestCase {
 
         OrderedHashtable<String, String> testTable = new OrderedHashtable<String, String>();
 
-        //LocalizationUtils.parseAndAdd(testTable,"string.0=#this line should be all comment", 0);
         LocalizationUtils.parseAndAdd(testTable,"string.1=this line should be cutoff here# this is bad if present", 0);
         LocalizationUtils.parseAndAdd(testTable,"string.2=this line should be cutoff here after the space # this is bad is present", 0);
         LocalizationUtils.parseAndAdd(testTable,"string.3=this line should all be here#", 0);
@@ -100,8 +99,11 @@ public class LocalizationTests extends TestCase {
         LocalizationUtils.parseAndAdd(testTable,"string.5=this line should be all here \\# including this \\# and this", 0);
         LocalizationUtils.parseAndAdd(testTable,"string.6=this line should be here \\# and this# not this", 0);
         LocalizationUtils.parseAndAdd(testTable,"string.7=this line should be here \\# and this# not this \\# not this either", 0);
+        LocalizationUtils.parseAndAdd(testTable,"string.8=we have a hash \\# and a newline \\n", 0);
+        LocalizationUtils.parseAndAdd(testTable,"string.9=this line should all be here with the hash \\#", 0);
+        // make sure doesn't crash
+        LocalizationUtils.parseAndAdd(testTable,"# this is the whole line", 0);
 
-        //assertTrue("Line 1 passed", testTable.get("string.0").equals(""));
         assertTrue("Line 1 failed: " + testTable.get("string.1"), testTable.get("string.1").equals("this line should be cutoff here"));
         assertTrue("Line 2 failed: " + testTable.get("string.2"), testTable.get("string.2").equals("this line should be cutoff here after the space "));
         assertTrue("Line 3 failed: " + testTable.get("string.3"), testTable.get("string.3").equals("this line should all be here"));
@@ -109,6 +111,8 @@ public class LocalizationTests extends TestCase {
         assertTrue("Line 5 failed: " + testTable.get("string.5"), testTable.get("string.5").equals("this line should be all here # including this # and this"));
         assertTrue("Line 6 failed: " + testTable.get("string.6"), testTable.get("string.6").equals("this line should be here # and this"));
         assertTrue("Line 7 failed: " + testTable.get("string.7"), testTable.get("string.7").equals("this line should be here # and this"));
+        assertTrue("Line 8 failed: " + testTable.get("string.8"), testTable.get("string.8").equals("we have a hash # and a newline \n"));
+        assertTrue("Line 9 failed: " + testTable.get("string.9"), testTable.get("string.9").equals("this line should all be here with the hash #"));
 
 
     }
