cae803d2016606f3d3e6ffc62145e4b82a5e9dc2
Phillip Mates <pmates@dimagi.com>
2015-06-19 14:25:34 -0400

    Style and linting

--- tests/test/org/commcare/util/test/CasePurgeFilterTests.java
+++ tests/test/org/commcare/util/test/CasePurgeFilterTests.java
@@ -17,21 +17,25 @@ import org.junit.Test;
 
 public class CasePurgeFilterTests {
 
-    Case a,b,c,d,e;
-    DummyIndexedStorageUtility<Case> storage;
-    String owner;
-    String groupOwner;
-    String otherOwner;
-    Vector<String> groupOwned;
-    Vector<String> userOwned;
+    private Case a;
+    private Case b;
+    private Case c;
+    private Case d;
+    private Case e;
+    private DummyIndexedStorageUtility<Case> storage;
+    private String owner;
+    private String groupOwner;
+    private String otherOwner;
+    private Vector<String> groupOwned;
+    private Vector<String> userOwned;
 
 
     @Before
     public void setUp() throws Exception {
 
-        storage =  new DummyIndexedStorageUtility<Case>(Case.class);
+        storage = new DummyIndexedStorageUtility<Case>(Case.class);
 
-        owner ="owner";
+        owner = "owner";
         otherOwner = "otherowner";
         groupOwner = "groupowned";
 
@@ -42,19 +46,19 @@ public class CasePurgeFilterTests {
         groupOwned.addElement(owner);
         groupOwned.addElement(groupOwner);
 
-        a = new Case("a","a");
+        a = new Case("a", "a");
         a.setCaseId("a");
         a.setUserId(owner);
-        b = new Case("b","b");
+        b = new Case("b", "b");
         b.setCaseId("b");
         b.setUserId(owner);
-        c = new Case("c","c");
+        c = new Case("c", "c");
         c.setCaseId("c");
         c.setUserId(owner);
-        d = new Case("d","d");
+        d = new Case("d", "d");
         d.setCaseId("d");
         d.setUserId(owner);
-        e = new Case("e","e");
+        e = new Case("e", "e");
         e.setCaseId("e");
         e.setUserId(groupOwner);
     }
@@ -67,14 +71,14 @@ public class CasePurgeFilterTests {
             storage.write(c);
             storage.write(d);
 
-            int[] present = new int[] {a.getID(), c.getID(), b.getID(), d.getID()};
-            int[] toRemove = new int[] {  };
+            int[] present = new int[]{a.getID(), c.getID(), b.getID(), d.getID()};
+            int[] toRemove = new int[]{};
 
             Vector<Integer> removed = storage.removeAll(new CasePurgeFilter(storage, userOwned));
             testOutcome(storage, present, toRemove);
             testRemovedClaim(removed, toRemove);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             fail("Unexpected exception " + e.getMessage());
         }
@@ -89,14 +93,14 @@ public class CasePurgeFilterTests {
             storage.write(c);
             storage.write(d);
 
-            int[] present = new int[] {a.getID(), c.getID(), d.getID()};
-            int[] toRemove = new int[] { b.getID()};
+            int[] present = new int[]{a.getID(), c.getID(), d.getID()};
+            int[] toRemove = new int[]{b.getID()};
 
             Vector<Integer> removed = storage.removeAll(new CasePurgeFilter(storage, userOwned));
             testOutcome(storage, present, toRemove);
             testRemovedClaim(removed, toRemove);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             fail("Unexpected exception " + e.getMessage());
         }
@@ -107,7 +111,7 @@ public class CasePurgeFilterTests {
         b.setUserId(otherOwner);
         c.setUserId(otherOwner);
 
-        d.setIndex("b","b",b.getCaseId());
+        d.setIndex("b", "b", b.getCaseId());
 
         try {
             storage.write(a);
@@ -115,14 +119,14 @@ public class CasePurgeFilterTests {
             storage.write(c);
             storage.write(d);
 
-            int[] present = new int[] {a.getID(), b.getID(), d.getID()};
-            int[] toRemove = new int[] { c.getID()};
+            int[] present = new int[]{a.getID(), b.getID(), d.getID()};
+            int[] toRemove = new int[]{c.getID()};
 
             Vector<Integer> removed = storage.removeAll(new CasePurgeFilter(storage, userOwned));
             testOutcome(storage, present, toRemove);
             testRemovedClaim(removed, toRemove);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             fail("Unexpected exception " + e.getMessage());
         }
@@ -137,14 +141,14 @@ public class CasePurgeFilterTests {
             storage.write(d);
             storage.write(e);
 
-            int[] present = new int[] {a.getID(), c.getID(), d.getID(), b.getID(), e.getID()};
-            int[] toRemove = new int[] {};
+            int[] present = new int[]{a.getID(), c.getID(), d.getID(), b.getID(), e.getID()};
+            int[] toRemove = new int[]{};
 
             Vector<Integer> removed = storage.removeAll(new CasePurgeFilter(storage, groupOwned));
             testOutcome(storage, present, toRemove);
             testRemovedClaim(removed, toRemove);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             fail("Unexpected exception " + e.getMessage());
         }
@@ -162,14 +166,14 @@ public class CasePurgeFilterTests {
             storage.write(c);
             storage.write(d);
 
-            int[] present = new int[] {a.getID(), c.getID()};
-            int[] toRemove = new int[] { b.getID(), d.getID() };
+            int[] present = new int[]{a.getID(), c.getID()};
+            int[] toRemove = new int[]{b.getID(), d.getID()};
 
             Vector<Integer> removed = storage.removeAll(new CasePurgeFilter(storage));
             testOutcome(storage, present, toRemove);
             testRemovedClaim(removed, toRemove);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             fail("Unexpected exception " + e.getMessage());
         }
@@ -186,21 +190,21 @@ public class CasePurgeFilterTests {
             storage.write(c);
             storage.write(d);
 
-            int[] present = new int[] {a.getID(), b.getID(), c.getID(), d.getID()};
-            int[] toRemove = new int[] { };
+            int[] present = new int[]{a.getID(), b.getID(), c.getID(), d.getID()};
+            int[] toRemove = new int[]{};
 
             Vector<Integer> removed = storage.removeAll(new CasePurgeFilter(storage));
             testOutcome(storage, present, toRemove);
             testRemovedClaim(removed, toRemove);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             fail("Unexpected exception " + e.getMessage());
         }
     }
 
     @Test
-    public void testDependenceDirection() {;
+    public void testDependenceDirection() {
         d.setIndex("b", "b", b.getCaseId());
         d.setClosed(true);
 
@@ -210,14 +214,14 @@ public class CasePurgeFilterTests {
             storage.write(c);
             storage.write(d);
 
-            int[] present = new int[] {a.getID(), b.getID(), c.getID()};
-            int[] toRemove = new int[] {d.getID()};
+            int[] present = new int[]{a.getID(), b.getID(), c.getID()};
+            int[] toRemove = new int[]{d.getID()};
 
             Vector<Integer> removed = storage.removeAll(new CasePurgeFilter(storage));
             testOutcome(storage, present, toRemove);
             testRemovedClaim(removed, toRemove);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             fail("Unexpected exception " + e.getMessage());
         }
@@ -235,14 +239,14 @@ public class CasePurgeFilterTests {
             storage.write(c);
             storage.write(d);
 
-            int[] present = new int[] {a.getID(), c.getID()};
-            int[] toRemove = new int[] {b.getID(), d.getID()};
+            int[] present = new int[]{a.getID(), c.getID()};
+            int[] toRemove = new int[]{b.getID(), d.getID()};
 
             Vector<Integer> removed = storage.removeAll(new CasePurgeFilter(storage));
             testOutcome(storage, present, toRemove);
             testRemovedClaim(removed, toRemove);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             fail("Unexpected exception " + e.getMessage());
         }
@@ -262,14 +266,14 @@ public class CasePurgeFilterTests {
             storage.write(d);
             storage.write(e);
 
-            int[] present = new int[] {a.getID(), c.getID(), b.getID(), d.getID(), e.getID()};
-            int[] toRemove = new int[] {};
+            int[] present = new int[]{a.getID(), c.getID(), b.getID(), d.getID(), e.getID()};
+            int[] toRemove = new int[]{};
 
             Vector<Integer> removed = storage.removeAll(new CasePurgeFilter(storage));
             testOutcome(storage, present, toRemove);
             testRemovedClaim(removed, toRemove);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             fail("Unexpected exception " + e.getMessage());
         }
@@ -289,14 +293,14 @@ public class CasePurgeFilterTests {
             storage.write(d);
             storage.write(e);
 
-            int[] present = new int[] {a.getID(), c.getID(), b.getID(), d.getID()};
-            int[] toRemove = new int[] {e.getID()};
+            int[] present = new int[]{a.getID(), c.getID(), b.getID(), d.getID()};
+            int[] toRemove = new int[]{e.getID()};
 
             Vector<Integer> removed = storage.removeAll(new CasePurgeFilter(storage));
             testOutcome(storage, present, toRemove);
             testRemovedClaim(removed, toRemove);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             fail("Unexpected exception " + e.getMessage());
         }
@@ -316,14 +320,14 @@ public class CasePurgeFilterTests {
             storage.write(d);
             storage.write(e);
 
-            int[] present = new int[] {a.getID(), c.getID(), b.getID()};
-            int[] toRemove = new int[] {d.getID(), e.getID()};
+            int[] present = new int[]{a.getID(), c.getID(), b.getID()};
+            int[] toRemove = new int[]{d.getID(), e.getID()};
 
             Vector<Integer> removed = storage.removeAll(new CasePurgeFilter(storage));
             testOutcome(storage, present, toRemove);
             testRemovedClaim(removed, toRemove);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             fail("Unexpected exception " + e.getMessage());
         }
@@ -340,14 +344,14 @@ public class CasePurgeFilterTests {
             storage.write(d);
             storage.write(e);
 
-            int[] present = new int[] {a.getID(), c.getID(), b.getID(), d.getID(), e.getID()};
-            int[] toRemove = new int[] {};
+            int[] present = new int[]{a.getID(), c.getID(), b.getID(), d.getID(), e.getID()};
+            int[] toRemove = new int[]{};
 
             Vector<Integer> removed = storage.removeAll(new CasePurgeFilter(storage));
             testOutcome(storage, present, toRemove);
             testRemovedClaim(removed, toRemove);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             fail("Unexpected exception " + e.getMessage());
         }
@@ -365,14 +369,14 @@ public class CasePurgeFilterTests {
             storage.write(d);
             storage.write(e);
 
-            int[] present = new int[] {c.getID(), b.getID(), d.getID()};
-            int[] toRemove = new int[] {a.getID(), e.getID()};
+            int[] present = new int[]{c.getID(), b.getID(), d.getID()};
+            int[] toRemove = new int[]{a.getID(), e.getID()};
 
             Vector<Integer> removed = storage.removeAll(new CasePurgeFilter(storage));
             testOutcome(storage, present, toRemove);
             testRemovedClaim(removed, toRemove);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             fail("Unexpected exception " + e.getMessage());
         }
@@ -392,14 +396,14 @@ public class CasePurgeFilterTests {
             storage.write(d);
             storage.write(e);
 
-            int[] present = new int[] {a.getID(), c.getID(), b.getID(), d.getID(), e.getID()};
-            int[] toRemove = new int[] {};
+            int[] present = new int[]{a.getID(), c.getID(), b.getID(), d.getID(), e.getID()};
+            int[] toRemove = new int[]{};
 
             Vector<Integer> removed = storage.removeAll(new CasePurgeFilter(storage));
             testOutcome(storage, present, toRemove);
             testRemovedClaim(removed, toRemove);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             fail("Unexpected exception " + e.getMessage());
         }
@@ -420,14 +424,14 @@ public class CasePurgeFilterTests {
             storage.write(d);
             storage.write(e);
 
-            int[] present = new int[] {c.getID(), d.getID()};
-            int[] toRemove = new int[] {a.getID(), b.getID(), e.getID()};
+            int[] present = new int[]{c.getID(), d.getID()};
+            int[] toRemove = new int[]{a.getID(), b.getID(), e.getID()};
 
             Vector<Integer> removed = storage.removeAll(new CasePurgeFilter(storage));
             testOutcome(storage, present, toRemove);
             testRemovedClaim(removed, toRemove);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             fail("Unexpected exception " + e.getMessage());
         }
@@ -447,14 +451,14 @@ public class CasePurgeFilterTests {
             storage.write(d);
             storage.write(e);
 
-            int[] present = new int[] {b.getID(), d.getID()};
-            int[] toRemove = new int[] {a.getID(), c.getID(), e.getID()};
+            int[] present = new int[]{b.getID(), d.getID()};
+            int[] toRemove = new int[]{a.getID(), c.getID(), e.getID()};
 
             Vector<Integer> removed = storage.removeAll(new CasePurgeFilter(storage));
             testOutcome(storage, present, toRemove);
             testRemovedClaim(removed, toRemove);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             fail("Unexpected exception " + e.getMessage());
         }
@@ -474,14 +478,14 @@ public class CasePurgeFilterTests {
             storage.write(d);
             storage.write(e);
 
-            int[] present = new int[] {a.getID(), b.getID(), c.getID(), d.getID(), e.getID()};
-            int[] toRemove = new int[] {};
+            int[] present = new int[]{a.getID(), b.getID(), c.getID(), d.getID(), e.getID()};
+            int[] toRemove = new int[]{};
 
             Vector<Integer> removed = storage.removeAll(new CasePurgeFilter(storage));
             testOutcome(storage, present, toRemove);
             testRemovedClaim(removed, toRemove);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             fail("Unexpected exception " + e.getMessage());
         }
@@ -501,14 +505,14 @@ public class CasePurgeFilterTests {
             storage.write(d);
             storage.write(e);
 
-            int[] present = new int[] {b.getID(), c.getID(), d.getID()};
-            int[] toRemove = new int[] {a.getID(), e.getID()};
+            int[] present = new int[]{b.getID(), c.getID(), d.getID()};
+            int[] toRemove = new int[]{a.getID(), e.getID()};
 
             Vector<Integer> removed = storage.removeAll(new CasePurgeFilter(storage));
             testOutcome(storage, present, toRemove);
             testRemovedClaim(removed, toRemove);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             fail("Unexpected exception " + e.getMessage());
         }
@@ -529,53 +533,53 @@ public class CasePurgeFilterTests {
             storage.write(d);
             storage.write(e);
 
-            int[] present = new int[] {a.getID(), e.getID(), b.getID(), c.getID(), d.getID()};
-            int[] toRemove = new int[] {};
+            int[] present = new int[]{a.getID(), e.getID(), b.getID(), c.getID(), d.getID()};
+            int[] toRemove = new int[]{};
 
             Vector<Integer> removed = storage.removeAll(new CasePurgeFilter(storage));
             testOutcome(storage, present, toRemove);
             testRemovedClaim(removed, toRemove);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             fail("Unexpected exception " + e.getMessage());
         }
     }
 
-    public void testOutcome(IStorageUtility<Case> storage, int[] p, int[] g) {
+    void testOutcome(IStorageUtility<Case> storage, int[] p, int[] g) {
         Vector<Integer> present = atv(p);
         Vector<Integer> gone = atv(g);
 
-        for(IStorageIterator<Case> iterator = storage.iterate(); iterator.hasMore(); ) {
+        for (IStorageIterator<Case> iterator = storage.iterate(); iterator.hasMore(); ) {
             Integer id = iterator.peekID();
             present.removeElement(id);
-            if(gone.contains(id)) {
+            if (gone.contains(id)) {
                 fail("Case: " + iterator.nextRecord().getCaseId() + " not purged");
             }
             iterator.nextID();
         }
-        if(present.size() > 0) {
+        if (present.size() > 0) {
             fail("No case with index " + present.firstElement().intValue() + " in testdb");
         }
     }
 
     private void testRemovedClaim(Vector<Integer> removed, int[] toRemove) {
-        if(removed.size() != toRemove.length) {
+        if (removed.size() != toRemove.length) {
             fail("storage purge returned incorrect size of returned items");
         }
 
-        for(int i = 0 ; i < toRemove.length; ++i) {
-            removed.removeElement(DataUtil.integer(toRemove[i]));
+        for (int aToRemove : toRemove) {
+            removed.removeElement(DataUtil.integer(aToRemove));
         }
-        if(removed.size() > 0) {
+        if (removed.size() > 0) {
             fail("storage purge returned incorrect set of removed items");
         }
     }
 
     private Vector<Integer> atv(int[] a) {
         Vector<Integer> ret = new Vector<Integer>(a.length);
-        for(int i = 0; i < a.length ; ++i) {
-            ret.addElement(DataUtil.integer(a[i]));
+        for (int anA : a) {
+            ret.addElement(DataUtil.integer(anA));
         }
         return ret;
     }
