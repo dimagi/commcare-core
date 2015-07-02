c9cedc35cfbef20e3adc247e70eea880666764a6
Will Pride <wpride@dimagi.om>
2015-07-02 14:21:56 -0400

    tests

--- tests/test/org/commcare/util/test/SqlSandboxTest.java
+++ tests/test/org/commcare/util/test/SqlSandboxTest.java
@@ -2,6 +2,7 @@ package org.commcare.util.test;
 
 import org.commcare.api.persistence.SqlSandbox;
 import org.commcare.api.util.UserDataUtils;
+import org.commcare.cases.model.Case;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -21,12 +22,12 @@ public class SqlSandboxTest {
     @Before
     public void setUp() throws Exception {
         sandbox = UserDataUtils.getStaticStorage(username);
-        UserDataUtils.parseIntoSandbox(this.getClass().getResourceAsStream("/case_create_purge.xml"), sandbox);
-        //owners = UserDataUtils.extractEntityOwners(sandbox);
+        UserDataUtils.parseIntoSandbox(this.getClass().getResourceAsStream("/ipm_restore.xml"), sandbox);
     }
 
     @Test
     public void test() {
-
+        Case readCase = sandbox.getCaseStorage().read(1);
+        System.out.println("read case: " + readCase + " name: " + readCase.getTypeId());
     }
 }
