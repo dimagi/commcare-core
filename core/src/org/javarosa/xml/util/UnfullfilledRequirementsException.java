a6dcf77577cb461b6f16dd68f846cc3e1642d50e
ALIZA STONE <amstone326@gmail.com>
2015-07-10 11:27:27 -0400

    Move check for Duplicate app installation earlier

--- core/src/org/javarosa/xml/util/UnfullfilledRequirementsException.java
+++ core/src/org/javarosa/xml/util/UnfullfilledRequirementsException.java
@@ -10,6 +10,7 @@ public class UnfullfilledRequirementsException extends Exception {
 
     private int severity;
     private int requirement;
+    private boolean isDuplicateApp;
 
     /**
      * Version Numbers if version is incompatible *
@@ -22,6 +23,11 @@ public class UnfullfilledRequirementsException extends Exception {
 
     public UnfullfilledRequirementsException(String message, int severity, int requirement) {
         this(message, severity, requirement, -1, -1, -1, -1);
+    }
+
+    public UnfullfilledRequirementsException(String message, int severity, boolean isDuplicateApp) {
+        this(message, severity, -1, -1, -1, -1, -1);
+        this.isDuplicateApp = isDuplicateApp;
     }
 
     /**
@@ -69,5 +75,9 @@ public class UnfullfilledRequirementsException extends Exception {
 
     public int getRequirementCode() {
         return requirement;
+    }
+
+    public boolean isDuplicateException() {
+        return this.isDuplicateApp;
     }
 }
