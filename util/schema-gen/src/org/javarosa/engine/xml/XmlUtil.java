208ea31cf214c0d624b0e7765828b15c4d00f3a8
Clayton Sims <csims@dimagi.com>
2015-07-14 14:07:19 -0400

    new exception handling

--- util/schema-gen/src/org/javarosa/engine/xml/XmlUtil.java
+++ util/schema-gen/src/org/javarosa/engine/xml/XmlUtil.java
@@ -49,11 +49,7 @@ public class XmlUtil {
             DocumentBuilder db = dbf.newDocumentBuilder();
             InputSource is = new InputSource(new StringReader(in));
             return db.parse(is);
-        } catch (ParserConfigurationException e) {
-            throw new RuntimeException(e);
-        } catch (SAXException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
+        } catch (ParserConfigurationException | SAXException | IOException e) {
             throw new RuntimeException(e);
         }
     }
