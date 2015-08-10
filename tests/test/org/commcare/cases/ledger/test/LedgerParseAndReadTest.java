ccd811d59d68ad2a7854cdacb85839ad6ba43636
Phillip Mates <pmates@dimagi.com>
2015-08-10 19:01:12 -0400

    Test ledger queries on missing entries

--- tests/test/org/commcare/cases/ledger/test/LedgerParseAndReadTest.java
+++ tests/test/org/commcare/cases/ledger/test/LedgerParseAndReadTest.java
@@ -1,16 +1,19 @@
 package org.commcare.cases.ledger.test;
 
 import org.commcare.test.utils.TestLedgerInitializer;
-import org.commcare.test.utils.XmlComparator;
 import org.commcare.util.mocks.MockDataUtils;
 import org.commcare.util.mocks.MockUserDataSandbox;
+import org.javarosa.core.model.condition.EvaluationContext;
 import org.javarosa.core.model.instance.ExternalDataInstance;
 import org.javarosa.model.xform.DataModelSerializer;
+import org.javarosa.xpath.XPathParseTool;
+import org.javarosa.xpath.expr.XPathExpression;
+import org.javarosa.xpath.expr.XPathFuncExpr;
+import org.javarosa.xpath.parser.XPathSyntaxException;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.kxml2.kdom.Document;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
@@ -18,32 +21,63 @@ import java.io.IOException;
  * @author Phillip Mates (pmates@dimagi.com)
  */
 public class LedgerParseAndReadTest {
+    private static final String LEDGER_INSTANCE = "jr://instance/ledgerdb";
     private MockUserDataSandbox sandbox;
+    private EvaluationContext evalContext;
 
     @Before
     public void setUp() {
         sandbox = MockDataUtils.getStaticStorage();
-    }
 
-    @Test
-    public void readLedger() {
         MockDataUtils.parseIntoSandbox(this.getClass().getResourceAsStream("/ledger_create_basic.xml"), sandbox);
 
-        byte[] parsedDb = dumpLedger("jr://instance/ledgerdb");
-        Document parsed = XmlComparator.getDocumentFromStream(new ByteArrayInputStream(parsedDb));
-
-
+        loadLedgerIntoSandbox();
+        evalContext = MockDataUtils.getInstanceContexts(this.sandbox, "ledger", "jr://instance/ledgerdb");
     }
 
-    private byte[] dumpLedger(String instanceRef) {
+    private void loadLedgerIntoSandbox() {
         try {
             ByteArrayOutputStream bos = new ByteArrayOutputStream();
             DataModelSerializer s = new DataModelSerializer(bos, new TestLedgerInitializer(sandbox));
 
-            s.serialize(new ExternalDataInstance(instanceRef, "ledger"), null);
-            return bos.toByteArray();
+            s.serialize(new ExternalDataInstance(LEDGER_INSTANCE, "ledger"), null);
         } catch (IOException e) {
             throw new RuntimeException(e.getMessage());
         }
+    }
+
+    @Test
+    public void queryExistingLedgerPath() {
+        Assert.assertTrue(xpathEval(evalContext, "instance('ledger')/ledgerdb/ledger[@entity-id='market_basket']/section[@section-id='edible_stock']/entry[@id='beans']", 8.0));
+    }
+
+    @Test
+    public void queryMissingLedgerPath() {
+        Assert.assertTrue(xpathEval(evalContext, "instance('ledger')/ledgerdb/ledger[@entity-id='H_mart']", ""));
+        Assert.assertTrue(xpathEval(evalContext, "instance('ledger')/ledgerdb/ledger[@entity-id='market_basket']/section[@section-id='amphibious_stock']", ""));
+        Assert.assertTrue(xpathEval(evalContext, "instance('ledger')/ledgerdb/ledger[@entity-id='market_basket']/section[@section-id='cleaning_stock']/entry[@id='bleach']", ""));
+
+        Assert.assertTrue(xpathEval(evalContext, "instance('ledger')/ledgerdb/ledger[@entity-id='H_mart']/section[@section-id='edible_stock']/entry[@id='beans']", ""));
+        Assert.assertTrue(xpathEval(evalContext, "instance('ledger')/ledgerdb/ledger[@entity-id='market_basket']/section[@section-id='amphibious_stock']/entry[@id='beans']", ""));
+    }
+
+    private static boolean xpathEval(EvaluationContext evalContext,
+                                      String input,
+                                      Object expectedOutput) {
+        XPathExpression expr;
+        try {
+            expr = XPathParseTool.parseXPath(input);
+        } catch (XPathSyntaxException e) {
+            e.printStackTrace();
+            return false;
+        }
+        Object output;
+        try {
+            output = XPathFuncExpr.unpack(expr.eval(evalContext));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return expectedOutput.equals(output);
     }
 }
