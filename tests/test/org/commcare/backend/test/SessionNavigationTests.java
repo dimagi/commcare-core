7f983411882ba019677f3b813b883b1ea51d0623
ALIZA STONE <amstone326@gmail.com>
2015-08-17 18:49:46 -0400

    tests for a module taht needs case before command

--- tests/test/org/commcare/backend/test/SessionNavigationTests.java
+++ tests/test/org/commcare/backend/test/SessionNavigationTests.java
@@ -36,18 +36,42 @@ public class SessionNavigationTests {
         Assert.assertEquals(session.getNeededData(), SessionFrame.STATE_DATUM_VAL);
         Assert.assertEquals(session.getNeededDatum().getDataId(), "case_id");
 
-        // Stepping back should go back only 1 level
+        // Stepping back after choosing a command should go back only 1 level
         session.stepBack();
         Assert.assertEquals(session.getNeededData(), SessionFrame.STATE_COMMAND_ID);
         Assert.assertEquals(session.getCommand(), "m0");
+
+        // After choosing registration form, should need computed case id
+        session.setCommand("m0-f0");
+        Assert.assertEquals(session.getNeededData(), SessionFrame.STATE_DATUM_COMPUTED);
+
+        //TODO: Figure out how to run a datum compute from here
     }
 
     @Test
     public void testNeedsCaseFirst() {
+        SessionWrapper session = mApp.getSession();
 
+        // Before anything is done in the session, should need a command
+        Assert.assertEquals(session.getNeededData(), SessionFrame.STATE_COMMAND_ID);
+
+        // After setting first command to m2, should need a case id, because both forms within m2 need one
+        session.setCommand("m2");
+        Assert.assertEquals(session.getNeededData(), SessionFrame.STATE_DATUM_VAL);
+        Assert.assertEquals(session.getNeededDatum().getDataId(), "case_id");
+
+        // After setting case id, should need to choose a form
+        session.setDatum("case_id", "case_two");
+        Assert.assertEquals(session.getNeededData(), SessionFrame.STATE_COMMAND_ID);
+
+        // Should be ready to go after choosing a form
+        session.setCommand("m2-f1");
+        Assert.assertEquals(session.getNeededData(), null);
     }
 
-
-
+    @Test
+    public void testStepBack() {
+        //TODO: test stepBack() with many different initial configurations of the stack
+    }
 
 }
