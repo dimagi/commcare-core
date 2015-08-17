f387df4f943695de6e6b5c37e9c635b072cd3ec6
ALIZA STONE <amstone326@gmail.com>
2015-08-17 18:10:43 -0400

    tests for a module that needs command before case

--- tests/test/org/commcare/backend/test/SessionNavigationTests.java
+++ tests/test/org/commcare/backend/test/SessionNavigationTests.java
@@ -20,12 +20,34 @@ public class SessionNavigationTests {
     }
 
     @Test
-    public void testBasicSessionWalk() {
+    public void testNeedsCommandFirst() {
         SessionWrapper session = mApp.getSession();
 
         // Before anything is done in the session, should need a command
         Assert.assertEquals(session.getNeededData(), SessionFrame.STATE_COMMAND_ID);
 
+        // After setting first command to m0, should still need another command because the 3 forms
+        // within m0 have different datum needs, so will prioritize getting the next command first
         session.setCommand("m0");
+        Assert.assertEquals(session.getNeededData(), SessionFrame.STATE_COMMAND_ID);
+
+        // After choosing the form, will need a case id
+        session.setCommand("m0-f1");
+        Assert.assertEquals(session.getNeededData(), SessionFrame.STATE_DATUM_VAL);
+        Assert.assertEquals(session.getNeededDatum().getDataId(), "case_id");
+
+        // Stepping back should go back only 1 level
+        session.stepBack();
+        Assert.assertEquals(session.getNeededData(), SessionFrame.STATE_COMMAND_ID);
+        Assert.assertEquals(session.getCommand(), "m0");
     }
+
+    @Test
+    public void testNeedsCaseFirst() {
+
+    }
+
+
+
+
 }
