8bbdc00cf1b1706939ac7984fcc068cc404b5878
Phillip Mates <pmates@dimagi.com>
2015-08-24 20:04:07 -0400

    TODO message

--- backend/src/org/commcare/resources/model/InstallCancelled.java
+++ backend/src/org/commcare/resources/model/InstallCancelled.java
@@ -1,5 +1,6 @@
 package org.commcare.resources.model;
 
+// TODO PLM: Have installers that download files check cancellation on big downloads
 public interface InstallCancelled {
     /**
      * @return was the resource install process cancelled by the user or system?
