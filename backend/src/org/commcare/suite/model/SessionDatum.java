8db50f5d6ee12706346f7e2ee0e287857928c326
ALIZA STONE <amstone326@gmail.com>
2015-09-12 20:26:39 -0400

    comments

--- backend/src/org/commcare/suite/model/SessionDatum.java
+++ backend/src/org/commcare/suite/model/SessionDatum.java
@@ -74,10 +74,17 @@ public class SessionDatum implements Externalizable {
         return nodeset;
     }
 
+    /**
+     * the ID of a detail that structures the screen for selecting an item from the nodeset
+     */
     public String getShortDetail() {
         return shortDetail;
     }
 
+    /**
+     * the ID of a detail that will show a selected item for confirmation. If not present,
+     * no confirmation screen is shown after item selection
+     */
     public String getLongDetail() {
         return longDetail;
     }
