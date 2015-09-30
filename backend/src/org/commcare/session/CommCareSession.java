c01a878581e01a8b692a6f79d94c4116e455c75f
ALIZA STONE <amstone326@gmail.com>
2015-09-30 15:01:12 -0400

    revert to original method name

--- backend/src/org/commcare/session/CommCareSession.java
+++ backend/src/org/commcare/session/CommCareSession.java
@@ -129,7 +129,7 @@ public class CommCareSession {
         return entries;
     }
 
-    protected OrderedHashtable getCollectedDatums() {
+    protected OrderedHashtable getData() {
         return collectedDatums;
     }
 
