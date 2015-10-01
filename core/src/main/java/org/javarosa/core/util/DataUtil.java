756e466fae37005ecfebc8801812248cd7ce25c8
Phillip Mates <pmates@dimagi.com>
2015-10-01 12:35:54 -0400

    Comment Integer cache method

--- core/src/main/java/org/javarosa/core/util/DataUtil.java
+++ core/src/main/java/org/javarosa/core/util/DataUtil.java
@@ -1,6 +1,3 @@
-/**
- *
- */
 package org.javarosa.core.util;
 
 import java.util.Vector;
@@ -16,16 +13,27 @@ public class DataUtil {
 
     static UnionLambda unionLambda = new UnionLambda();
 
+    /**
+     * Get Integer object that corresponds to int argument from a
+     * pre-computed table, or build a new instance.
+     *
+     * @return Cached or new Integer instance that correpsonds to ivalue argument
+     */
     public static Integer integer(int ivalue) {
+        // lazily populate Integer cache
         if (iarray == null) {
             iarray = new Integer[high - low];
             for (int i = 0; i < iarray.length; ++i) {
                 iarray[i] = new Integer(i + low);
             }
         }
-        return ivalue < high && ivalue >= low ? iarray[ivalue + offset] : new Integer(ivalue);
-    }
 
+        if (ivalue < high && ivalue >= low) {
+            return iarray[ivalue + offset];
+        } else {
+            return new Integer(ivalue);
+        }
+    }
 
     public static <T> Vector<T> union(Vector<T> a, Vector<T> b) {
         return unionLambda.union(a, b);
