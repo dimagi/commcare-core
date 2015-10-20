b728f184286412b45461b86d3160f5a95e61dc46
Clayton Sims <csims@dimagi.com>
2015-10-20 20:38:07 +0530

    Removed unused legacy code

--- cases/src/org/commcare/cases/util/CasePurgeFilter.java
+++ cases/src/org/commcare/cases/util/CasePurgeFilter.java
@@ -21,32 +21,24 @@ import java.util.Vector;
  * @author ctsims
  */
 public class CasePurgeFilter extends EntityFilter<Case> {
-
     /**
-     * Open and owned by someone on the phone
+     * Owned by the user or a group
      */
-    private static final String STATUS_LIVE = "L";
-    /**
-     * Purgable *
-     */
-    private static final String STATUS_DEAD = "D";
-    /**
-     * Closed or not owned (NOTE: No longer used) *
-     */
-    private static final String STATUS_CLOSED = "C";
-    /**
-     * Not currently alive, and dependent on another case *
-     */
-    private static final String STATUS_ABANDONED = "A";
-
-
-
     private static final int STATUS_OWNED = 1;
 
+    /**
+     * Should be included in the set of cases seen by the user
+     */
     private static final int STATUS_RELEVANT= 2;
 
+    /**
+     * Isn't precluded from being included in a sync for any reason
+     */
     private static final int STATUS_AVAILABLE = 4;
 
+    /**
+     * Should remain on the phone.
+     */
     private static final int STATUS_ALIVE = 8;
 
     private static final int STATUS_OPEN = 16;
@@ -70,136 +62,6 @@ public class CasePurgeFilter extends EntityFilter<Case> {
      */
     public CasePurgeFilter(IStorageUtilityIndexed<Case> caseStorage, Vector<String> owners) {
         setIdsToRemoveWithNewExtensions(caseStorage, owners);
-    }
-
-    private void setIdsToRemoveNew(IStorageUtilityIndexed<Case> caseStorage, Vector<String> owners) {
-        //Create a DAG. The Index will be the case GUID. The Nodes will be a string array containing
-        //[CASE_STATUS, string(storageid)]
-        //CASE_STATUS is enumerated as one of STATUS_LIVE, STATUS_DEAD, or STATUS_ABANDONED
-        DAG<String, String[], String> g = new DAG<String, String[], String>();
-
-        Vector<CaseIndex> indexHolder = new Vector<CaseIndex>();
-
-        //Pass 1:
-        //Create a DAG which contains all of the cases on the phone as nodes, and has a directed
-        //edge for each index (from the 'child' case pointing to the 'parent' case) with the
-        //appropriate relationship tagged
-        for (IStorageIterator<Case> i = caseStorage.iterate(); i.hasMore(); ) {
-            Case c = i.nextRecord();
-            boolean owned = true;
-            if (owners != null) {
-                owned = owners.contains(c.getUserId());
-            }
-
-            //In order to deal with multiple indices pointing to the same case
-            //with different relationships, we'll need to traverse once to eliminate any
-            //ambiguity (TODO: How do we speed this up? Do we need to?)
-            for (CaseIndex index : c.getIndices()) {
-                CaseIndex toReplace = null;
-                boolean skip = false;
-                for (CaseIndex existing : indexHolder) {
-                    if (existing.getTarget().equals(index.getTarget())) {
-                        if (existing.getRelationship().equals(CaseIndex.RELATIONSHIP_EXTENSION) && !index.getRelationship().equals(CaseIndex.RELATIONSHIP_EXTENSION)) {
-                            toReplace = existing;
-                        } else {
-                            skip = true;
-                        }
-                        break;
-                    }
-                }
-                if (toReplace != null) {
-                    indexHolder.removeElement(toReplace);
-                }
-                if (!skip) {
-                    indexHolder.addElement(index);
-                }
-            }
-            String nodeStatus;
-            //Four cases, applied in order. One: closed and starts life dead
-            if (c.isClosed()) {
-                nodeStatus = STATUS_DEAD;
-            } else if(!owned) {
-                //two: unowned is dead unless it is an extension
-                if(hasExtension(indexHolder)) {
-                    nodeStatus = STATUS_ABANDONED;
-                } else {
-                    nodeStatus = STATUS_DEAD;
-                }
-            } else {
-                //Otherwise we need to see whether this case maintains any extension indices,
-                //if so, it's abandoned. Otherwise it's Alive
-                boolean abandoned = hasExtension(indexHolder);
-                if (abandoned) {
-                    nodeStatus = STATUS_ABANDONED;
-                } else {
-                    nodeStatus = STATUS_LIVE;
-                }
-            }
-
-
-            g.addNode(c.getCaseId(), new String[]{nodeStatus, String.valueOf(c.getID())});
-
-            for (CaseIndex index : indexHolder) {
-                g.setEdge(c.getCaseId(), index.getTarget(), index.getRelationship());
-            }
-            indexHolder.removeAllElements();
-        }
-
-        //Pass 2: Start at Sources (nodes which only produce edges) and walk up
-        //the tree, marking relevant nodes as alive
-        Stack<String> toProcess = g.getSources();
-        while (!toProcess.isEmpty()) {
-            //current node
-            String index = toProcess.pop();
-            String[] node = g.getNode(index);
-
-            //Walk all indexed nodes, adding them to the process stack.
-            //and update their liveness if necessary.
-            for (Edge<String, String> edge : g.getChildren(index)) {
-                if (node[0].equals(STATUS_LIVE)) {
-                    g.getNode(edge.i)[0] = STATUS_LIVE;
-                }
-                //add this to the process stack
-                //TODO: We're going to walk stuff redundantly this
-                //way. Can we optimize?
-                //If parent is already live, does
-                //that necessarily denote that its ancestors are alive?
-                toProcess.addElement(edge.i);
-            }
-        }
-
-        //Pass 2: Start at Sinks (nodes which only receive edges) and walk down the
-        //graphs, marking necessary nodes as alive
-        toProcess = g.getSinks();
-        while (!toProcess.isEmpty()) {
-            //current node
-            String index = toProcess.pop();
-            String[] node = g.getNode(index);
-
-            //Walk all indexing nodes.
-            for (Edge<String, String> edge : g.getParents(index)) {
-                if (g.getNode(edge.i)[0].equals(STATUS_ABANDONED) &&
-                        edge.e.equals(CaseIndex.RELATIONSHIP_EXTENSION) &&
-                        node[0].equals(STATUS_LIVE)) {
-
-                    g.getNode(edge.i)[0] = STATUS_LIVE;
-                }
-
-                //add this to the process stack
-                //TODO: We're going to walk stuff redundantly this
-                //way. Can we optimize?
-                toProcess.addElement(edge.i);
-            }
-        }
-
-        //Ok, so now just go through all nodes and signal that we need to remove anything
-        //that isn't live!
-        for (Enumeration iterator = g.getNodes(); iterator.hasMoreElements(); ) {
-            String[] node = (String[])iterator.nextElement();
-            if (!node[0].equals(STATUS_LIVE)) {
-                idsToRemove.addElement(Integer.valueOf(node[1]));
-            }
-        }
     }
 
     private void setIdsToRemoveWithNewExtensions(IStorageUtilityIndexed<Case> caseStorage, Vector<String> owners) {
@@ -267,7 +129,7 @@ public class CasePurgeFilter extends EntityFilter<Case> {
         markRelevant(g);
         markAvailable(g);
         markLive(g);
-
+    
         //Ok, so now just go through all nodes and signal that we need to remove anything
         //that isn't live!
         for (Enumeration<int[]> iterator = g.getNodes(); iterator.hasMoreElements(); ) {
