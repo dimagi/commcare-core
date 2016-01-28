68aa9a5ebf159d79910e4185830b25465b47c2d6
Jenny Schweers <jschweers@dimagi.com>
2016-01-28 12:34:45 -0500

    Clean up imports in GraphParser

--- backend/src/org/commcare/xml/GraphParser.java
+++ backend/src/org/commcare/xml/GraphParser.java
@@ -1,15 +1,8 @@
 package org.commcare.xml;
 
-import org.commcare.suite.model.Text;
-import org.commcare.suite.model.graph.Annotation;
-import org.commcare.suite.model.graph.BubbleSeries;
-import org.commcare.suite.model.graph.Configurable;
 import org.commcare.suite.model.graph.Graph;
-import org.commcare.suite.model.graph.XYSeries;
 import org.javarosa.xml.ElementParser;
 import org.javarosa.xml.util.InvalidStructureException;
-import org.javarosa.xpath.XPathParseTool;
-import org.javarosa.xpath.parser.XPathSyntaxException;
 import org.kxml2.io.KXmlParser;
 import org.xmlpull.v1.XmlPullParserException;
 
