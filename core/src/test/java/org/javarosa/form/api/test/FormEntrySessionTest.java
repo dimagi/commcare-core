ef4e4e5f52018652b808e68445fb711d7fd16f14
Phillip Mates <pmates@dimagi.com>
2016-03-18 01:33:24 -0400

    Remove commented out tests

--- core/src/test/java/org/javarosa/form/api/test/FormEntrySessionTest.java
+++ core/src/test/java/org/javarosa/form/api/test/FormEntrySessionTest.java
@@ -27,31 +27,4 @@ public class FormEntrySessionTest {
         expectedOutput = new Vector<>(Arrays.asList("(a)", "(\\(b c\\))", "(\\(d\\))"));
         Assert.assertEquals(expectedOutput, FormEntrySession.splitTopParens(input));
     }
-
-    /*@Test
-    public void testFormEntrySessionStringSerialization() {
-        FormEntrySession formEntrySession = new FormEntrySession();
-        formEntrySession.addValueSet(FormIndex.createBeginningOfFormIndex(), "foo");
-        formEntrySession.addNewRepeat(FormIndex.createBeginningOfFormIndex());
-        formEntrySession.addValueSet(FormIndex.createEndOfFormIndex(), "bar");
-        String sessionAsString = formEntrySession.toString();
-        Assert.assertEquals(sessionAsString, FormEntrySession.fromString(sessionAsString).toString());
-    }
-
-    @Test
-    public void testAddingDuplicate() {
-        FormEntrySession formEntrySession = new FormEntrySession();
-        formEntrySession.addValueSet(FormIndex.createBeginningOfFormIndex(), "foo");
-        formEntrySession.addValueSet(FormIndex.createBeginningOfFormIndex(), "foo");
-        Assert.assertEquals(1, formEntrySession.size());
-
-        FormIndex randomIndex = new FormIndex(FormIndex.createBeginningOfFormIndex(), 52, 2, null);
-        formEntrySession.addValueSet(randomIndex, "bar");
-        FormEntryAction secondAction = formEntrySession.peekAction();
-
-        formEntrySession.addValueSet(FormIndex.createBeginningOfFormIndex(), "foo");
-
-        Assert.assertEquals(2, formEntrySession.size());
-        Assert.assertEquals(secondAction, formEntrySession.peekAction());
-    }*/
 }
