54c6904daf8983d4edb5bb039fb59618c6158bbb
Phillip Mates <pmates@dimagi.com>
2016-03-23 09:32:58 -0400

    Allow for localizable error message to be raised

--- core/src/main/java/org/javarosa/xml/util/InvalidStructureException.java
+++ core/src/main/java/org/javarosa/xml/util/InvalidStructureException.java
@@ -1,5 +1,6 @@
 package org.javarosa.xml.util;
 
+import org.javarosa.core.services.locale.Localization;
 import org.kxml2.io.KXmlParser;
 
 /**
@@ -10,6 +11,8 @@ import org.kxml2.io.KXmlParser;
  * @author ctsims
  */
 public class InvalidStructureException extends Exception {
+    private String localizationKey;
+    private String[] localizationParameters;
 
     /**
      * @param message A Message associated with the error.
@@ -30,6 +33,27 @@ public class InvalidStructureException extends Exception {
 
     public InvalidStructureException(String message) {
         super(message);
+    }
+
+    private InvalidStructureException(String localizationKey, String[] localizationParameters, String message) {
+        super(message);
+        this.localizationKey = localizationKey;
+        this.localizationParameters = localizationParameters;
+    }
+
+    public String getLocalizedMessage() {
+        if (localizationKey != null) {
+            return Localization.get(localizationKey, localizationParameters);
+        } else {
+            return getMessage();
+        }
+    }
+
+    public static InvalidStructureException localizableInvalidStructureException(String localizationKey,
+                                                                                 String[] localizationParameters,
+                                                                                 String defaultMessage,
+                                                                                 KXmlParser parser) {
+        return new InvalidStructureException(localizationKey, localizationParameters, defaultMessage);
     }
 
     public static InvalidStructureException readableInvalidStructureException(String message, KXmlParser parser) {
