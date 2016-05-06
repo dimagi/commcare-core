4ade61a5cdd92225c9904057e15e035bf628a8a4
Phillip Mates <pmates@dimagi.com>
2016-05-06 16:27:26 -0400

    Catch and rethrow unicode save errors to show to user

--- core/src/main/java/org/javarosa/xform/util/XFormSerializer.java
+++ core/src/main/java/org/javarosa/xform/util/XFormSerializer.java
@@ -3,6 +3,7 @@ package org.javarosa.xform.util;
 import org.kxml2.io.KXmlSerializer;
 import org.kxml2.kdom.Document;
 import org.kxml2.kdom.Element;
+import org.xmlpull.v1.XmlSerializer;
 
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
@@ -56,18 +57,36 @@ public class XFormSerializer {
      * Formats an XML document into a UTF-8 (no BOM) compatible format
      *
      * @return The raw bytes of the utf-8 encoded doc
-     * @throws IOException If there is an issue transferring the bytes to a byte stream.
-     * @throws UnsupportedEncodingException If the document contains values that are not UTF-8
-     * encoded.
+     * @throws IOException                           If there is an issue transferring
+     *                                               the bytes to a byte stream.
+     * @throws UnsupportedUnicodeSurrogatesException If the document contains values
+     *                                               that are not UTF-8 encoded.
      */
     public static byte[] getUtfBytesFromDocument(Document doc) throws IOException {
-        KXmlSerializer serializer = new KXmlSerializer();
+        KXmlSerializer serializer = new KXmlSerializer() {
+            @Override
+            public XmlSerializer text(String text) throws IOException {
+                try {
+                    return super.text(text);
+                } catch (IllegalArgumentException e) {
+                    // certain versions of Android have trouble encoding
+                    // unicode characters that require "surrogates".
+                    throw new UnsupportedUnicodeSurrogatesException(text);
+                }
+            }
+        };
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         Writer osw = new OutputStreamWriter(bos, "UTF-8");
         serializer.setOutput(osw);
         doc.write(serializer);
         serializer.flush();
         return bos.toByteArray();
+    }
+
+    public static class UnsupportedUnicodeSurrogatesException extends RuntimeException {
+        public UnsupportedUnicodeSurrogatesException(String message) {
+            super(message);
+        }
     }
 
 }
