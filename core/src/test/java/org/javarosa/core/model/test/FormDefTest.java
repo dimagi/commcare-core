6d3f2b497296f49fe010893438751b4058d58f2a
Will Pride <wpride@dimagi.com>
2016-06-20 14:57:02 -0400

    removeu nused

--- core/src/test/java/org/javarosa/core/model/test/FormDefTest.java
+++ core/src/test/java/org/javarosa/core/model/test/FormDefTest.java
@@ -532,21 +532,4 @@ public class FormDefTest {
         fec.jumpToIndex(FormIndex.createBeginningOfFormIndex());
         return fec;
     }
-
-    String readFile(String fileName) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(fileName));
-        try {
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
-
-            while (line != null) {
-                sb.append(line);
-                sb.append("\n");
-                line = br.readLine();
-            }
-            return sb.toString();
-        } finally {
-            br.close();
-        }
-    }
 }
