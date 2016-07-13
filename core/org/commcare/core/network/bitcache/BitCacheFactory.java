f9c2c7b50dcb5ff0066f53b336f0f3575873cdc0
Phillip Mates <pmates@dimagi.com>
2016-07-13 14:45:53 -0400

    Move bitcache and CryptUtil to core

---
+++ core/org/commcare/core/network/bitcache/BitCacheFactory.java
@@ -0,0 +1,19 @@
+package org.commcare.core.network.bitcache;
+
+import java.io.File;
+
+/**
+ * @author ctsims
+ */
+public class BitCacheFactory {
+    public static BitCache getCache(CacheDirSetup context, long estimatedSize) {
+        if (estimatedSize == -1 || estimatedSize > 1024 * 1024 * 4) {
+            return new FileBitCache(context);
+        }
+        return new MemoryBitCache();
+    }
+
+    public interface CacheDirSetup {
+        File getCacheDir();
+    }
+}
