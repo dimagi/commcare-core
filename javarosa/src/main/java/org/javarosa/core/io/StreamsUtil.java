4ab547e9417a020dfbcecb992648bc3a9f1b123c
Phillip Mates <pmates@dimagi.com>
2016-07-13 14:33:45 -0400

    Clean-up StreamsUtil

--- javarosa/src/main/java/org/javarosa/core/io/StreamsUtil.java
+++ javarosa/src/main/java/org/javarosa/core/io/StreamsUtil.java
@@ -9,8 +9,7 @@ public class StreamsUtil {
      * Write everything from input stream to output stream, byte by byte then
      * close the streams
      */
-    @SuppressWarnings("unused")
-    public static void writeFromInputToOutput(InputStream in, OutputStream out, long[] tally) throws InputIOException, OutputIOException {
+    private static void writeFromInputToOutputInner(InputStream in, OutputStream out) throws InputIOException, OutputIOException {
         //TODO: God this is naive
         int val;
         try {
@@ -24,7 +23,6 @@ public class StreamsUtil {
             } catch (IOException e) {
                 throw new StreamsUtil().new OutputIOException(e);
             }
-            incr(tally);
             try {
                 val = in.read();
             } catch (IOException e) {
@@ -34,12 +32,12 @@ public class StreamsUtil {
     }
 
     public static void writeFromInputToOutputSpecific(InputStream in, OutputStream out) throws InputIOException, OutputIOException {
-        writeFromInputToOutput(in, out, null);
+        writeFromInputToOutputInner(in, out);
     }
 
     public static void writeFromInputToOutput(InputStream in, OutputStream out) throws IOException {
         try {
-            writeFromInputToOutput(in, out, null);
+            writeFromInputToOutputInner(in, out);
         } catch (InputIOException e) {
             throw e.internal;
         } catch (OutputIOException e) {
@@ -47,39 +45,8 @@ public class StreamsUtil {
         }
     }
 
-    private static final int CHUNK_SIZE = 2048;
-
-    /**
-     * Write the byte array to the output stream
-     */
-    public static void writeToOutput(byte[] bytes, OutputStream out, long[] tally) throws IOException {
-        int offset = 0;
-        int remain = bytes.length;
-
-        while (remain > 0) {
-            int toRead = (remain < CHUNK_SIZE) ? remain : CHUNK_SIZE;
-            out.write(bytes, offset, toRead);
-            remain -= toRead;
-            offset += toRead;
-            if (tally != null) {
-                tally[0] += toRead;
-            }
-        }
-    }
-
-    @SuppressWarnings("unused")
-    public static void writeToOutput(byte[] bytes, OutputStream out) throws IOException {
-        writeToOutput(bytes, out, null);
-    }
-
-    private static void incr(long[] tally) {
-        if (tally != null) {
-            tally[0]++;
-        }
-    }
 
     //Unify the functional aspects here
-
     private abstract class DirectionalIOException extends IOException {
         final IOException internal;
 
