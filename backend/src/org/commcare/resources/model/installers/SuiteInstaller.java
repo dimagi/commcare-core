d703e82c1f7529701de5bf5e7863e8128e1c42bc
Phillip Mates <pmates@dimagi.com>
2016-07-15 10:52:44 -0400

    linting

--- backend/src/org/commcare/resources/model/installers/SuiteInstaller.java
+++ backend/src/org/commcare/resources/model/installers/SuiteInstaller.java
@@ -1,6 +1,3 @@
-/**
- *
- */
 package org.commcare.resources.model.installers;
 
 import org.commcare.resources.model.MissingMediaException;
@@ -24,7 +21,6 @@ import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Iterator;
 import java.util.Vector;
 
 /**
@@ -32,26 +28,26 @@ import java.util.Vector;
  */
 public class SuiteInstaller extends CacheInstaller<Suite> {
 
-    /* (non-Javadoc)
-     * @see org.commcare.resources.model.ResourceInitializer#initializeResource(org.commcare.resources.model.Resource)
-     */
+    @Override
     public boolean initialize(CommCareInstance instance) throws ResourceInitializationException {
         instance.registerSuite(storage().read(cacheLocation));
         return true;
     }
 
-    /* (non-Javadoc)
-     * @see org.commcare.resources.model.ResourceInitializer#requiresRuntimeInitialization()
-     */
+    @Override
     public boolean requiresRuntimeInitialization() {
         return true;
     }
 
+    @Override
     protected String getCacheKey() {
         return Suite.STORAGE_KEY;
     }
 
-    public boolean install(Resource r, ResourceLocation location, Reference ref, ResourceTable table, CommCareInstance instance, boolean upgrade) throws UnresolvedResourceException, UnfullfilledRequirementsException {
+    @Override
+    public boolean install(Resource r, ResourceLocation location, Reference ref,
+                           ResourceTable table, CommCareInstance instance,
+                           boolean upgrade) throws UnresolvedResourceException, UnfullfilledRequirementsException {
         if (location.getAuthority() == Resource.RESOURCE_AUTHORITY_CACHE) {
             //If it's in the cache, we should just get it from there
             return false;
@@ -70,20 +66,17 @@ public class SuiteInstaller extends CacheInstaller<Suite> {
 
                 table.commitCompoundResource(r, Resource.RESOURCE_STATUS_INSTALLED);
 
-                //TODOD:
-                //Add a resource location for r for its cache location
-                //so it can be uninstalled appropriately.
+                // TODO: Add a resource location for r for its cache location
+                // so it can be uninstalled appropriately.
                 return true;
             } catch (InvalidStructureException e) {
                 throw new UnresolvedResourceException(r, e.getMessage(), true);
             } catch (StorageFullException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
                 return false;
             } catch (IOException e) {
                 throw new UnreliableSourceException(r, e.getMessage());
             } catch (XmlPullParserException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
                 return false;
             } finally {
@@ -97,17 +90,18 @@ public class SuiteInstaller extends CacheInstaller<Suite> {
         }
     }
 
+    @Override
     public boolean verifyInstallation(Resource r, Vector<MissingMediaException> problems) {
-
-        SizeBoundUniqueVector sizeBoundProblems = (SizeBoundUniqueVector)problems;
+        SizeBoundUniqueVector<MissingMediaException> sizeBoundProblems =
+                (SizeBoundUniqueVector<MissingMediaException>)problems;
 
         InstallerUtil.checkMedia(r, Localization.get("icon.demo.path"), sizeBoundProblems, InstallerUtil.MediaType.IMAGE);
         InstallerUtil.checkMedia(r, Localization.get("icon.login.path"), sizeBoundProblems, InstallerUtil.MediaType.IMAGE);
 
         //Check to see whether the formDef exists and reads correctly
-        Suite mSuite;
+        Suite suite;
         try {
-            mSuite = storage().read(cacheLocation);
+            suite = storage().read(cacheLocation);
         } catch (Exception e) {
             e.printStackTrace();
             sizeBoundProblems.addElement(new MissingMediaException(r, "Suite did not properly save into persistent storage"));
@@ -116,18 +110,13 @@ public class SuiteInstaller extends CacheInstaller<Suite> {
         //Otherwise, we want to figure out if the form has media, and we need to see whether it's properly
         //available
         try {
-            Vector<Menu> menus = mSuite.getMenus();
-            Iterator e = menus.iterator();
-
-            while (e.hasNext()) {
-                Menu mMenu = (Menu)e.next();
-
-                String aURI = mMenu.getAudioURI();
+            for (Menu menu : suite.getMenus()) {
+                String aURI = menu.getAudioURI();
                 if (aURI != null) {
                     InstallerUtil.checkMedia(r, aURI, sizeBoundProblems, InstallerUtil.MediaType.AUDIO);
                 }
 
-                String iURI = mMenu.getImageURI();
+                String iURI = menu.getImageURI();
                 if (iURI != null) {
                     InstallerUtil.checkMedia(r, iURI, sizeBoundProblems, InstallerUtil.MediaType.IMAGE);
                 }
