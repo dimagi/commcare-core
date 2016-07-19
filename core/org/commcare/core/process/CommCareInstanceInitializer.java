114366d6aa0d86ba7a0c63caf536e6eaa6a83b35
Phillip Mates <pmates@dimagi.com>
2016-07-19 15:55:39 -0400

    Add a default

--- core/org/commcare/core/process/CommCareInstanceInitializer.java
+++ core/org/commcare/core/process/CommCareInstanceInitializer.java
@@ -154,7 +154,9 @@ public class CommCareInstanceInitializer extends InstanceInitializationFactory {
     public static class FixtureInitializationException extends RuntimeException {
 
         public FixtureInitializationException(String lookupReference) {
-            super(Localization.get("lookup.table.missing.error", lookupReference));
+            super(Localization.getWithDefault("lookup.table.missing.error",
+                    new String[] {lookupReference},
+                    "Unable to find lookup table: " + lookupReference));
         }
     }
 }
