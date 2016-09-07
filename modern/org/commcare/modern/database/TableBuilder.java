06c48dc80d0bc9278aaa51d1d1e11def75edbd32
Will Pride <wpride@dimagi.com>
2016-09-07 11:52:33 -0400

    ben

--- modern/org/commcare/modern/database/TableBuilder.java
+++ modern/org/commcare/modern/database/TableBuilder.java
@@ -180,7 +180,7 @@ public class TableBuilder {
 
     //sqlite doesn't like dashes
     public static String scrubName(String input) {
-        return input.replace("-", "dash").replace(".", "dot").replace("+", "plus");
+        return input.replace("-", "_dash_").replace(".", "_dot_").replace("+", "_plus_");
     }
 
     public static byte[] toBlob(Externalizable externalizable){
