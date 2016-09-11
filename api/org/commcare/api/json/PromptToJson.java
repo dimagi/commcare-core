c2200cd7bbcf4dcdaab05d445c9186f227be2ad7
Ben <rudolphben@gmail.com>
2016-09-11 17:01:49 -0400

    Properly parse datetime

--- api/org/commcare/api/json/PromptToJson.java
+++ api/org/commcare/api/json/PromptToJson.java
@@ -10,6 +10,7 @@ import org.javarosa.form.api.FormEntryCaption;
 import org.javarosa.form.api.FormEntryController;
 import org.javarosa.form.api.FormEntryModel;
 import org.javarosa.form.api.FormEntryPrompt;
+import org.joda.time.DateTime;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -140,7 +141,7 @@ public class PromptToJson {
                 obj.put("answer", answerValue.getDisplayText());
                 return;
             case Constants.DATATYPE_DATE_TIME:
-                obj.put("answer", ((Date) answerValue.getValue()).getTime());
+                obj.put("answer", new DateTime((Date) answerValue.getValue()).toString("yyyy-MM-dd'T'HH:mm:ssZZ"));
                 return;
             case Constants.DATATYPE_CHOICE:
                 Selection singleSelection = ((Selection) answerValue.getValue());
