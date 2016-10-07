69067c73a0f19c4655397e9d49ebe7b874d45685
Phillip Mates <pmates@dimagi.com>
2016-10-07 11:34:26 -0400

    Use InvalidResourceStructureException to signal invalid structure

---
+++ backend/src/org/commcare/resources/model/InvalidResourceStructureException.java
@@ -0,0 +1,12 @@
+package org.commcare.resources.model;
+
+/**
+ * Represents an install issue caused by resource having invalid content (like mismatched xml tag)
+ *
+ * @author Phillip Mates (pmates@dimagi.com)
+ */
+public class InvalidResourceStructureException extends RuntimeException {
+    public InvalidResourceStructureException(String msg) {
+        super(msg);
+    }
+}
