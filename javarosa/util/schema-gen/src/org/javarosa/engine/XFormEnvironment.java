76806f8462adef3a4a1e1afc0638bea5449f54f4
Phillip Mates <pmates@dimagi.com>
2016-11-09 10:52:54 -0200

    Add comments and clear up some code

--- javarosa/util/schema-gen/src/org/javarosa/engine/XFormEnvironment.java
+++ javarosa/util/schema-gen/src/org/javarosa/engine/XFormEnvironment.java
@@ -2,7 +2,6 @@ package org.javarosa.engine;
 
 import org.javarosa.core.model.FormDef;
 import org.javarosa.core.model.condition.EvaluationContext;
-import org.javarosa.core.model.condition.IFunctionHandler;
 import org.javarosa.core.model.instance.InstanceInitializationFactory;
 import org.javarosa.core.model.utils.DateUtils;
 import org.javarosa.engine.models.Action;
@@ -14,10 +13,8 @@ import org.javarosa.form.api.FormEntryModel;
 
 import java.util.Date;
 import java.util.Hashtable;
-import java.util.Vector;
 
 /**
- *
  * Run an XForm programatically for fun and profit.
  *
  * @author ctsims
@@ -33,8 +30,11 @@ public class XFormEnvironment {
 
     private Session session;
     private Mockup mockup;
-    private Date hardCodedDate;
-    boolean recording = true;
+
+    private boolean recording = true;
+
+    // stores date override of 'today()' / 'now()'
+    private Date hardCodedDate = new Date();
 
     public XFormEnvironment(FormDef form) {
         this.form = form;
@@ -56,7 +56,7 @@ public class XFormEnvironment {
     }
     
     public FormEntryController setup(InstanceInitializationFactory factory) {
-        form.setEvaluationContext(getEC());
+        form.setEvaluationContext(buildBaseEvaluationContext());
 
         form.initialize(true, factory, preferredLocale);
 
@@ -92,14 +92,21 @@ public class XFormEnvironment {
         return new MockupProviderFactory(mockup == null ? new Hashtable() : mockup.getInstances());
     }
 
-    private EvaluationContext getEC() {
+    /**
+     * Builds evaluation context that overrides 'today()' and 'now()' with
+     * custom date, if provided.
+     */
+    private EvaluationContext buildBaseEvaluationContext() {
         EvaluationContext ec = new EvaluationContext(null);
-        Date customDate = hardCodedDate;
+
+        // if present, load date from formplayer mockup (which allows for restoring formplayer state)
         if (mockup != null) {
-            customDate = mockup.getDate();
+            hardCodedDate = mockup.getDate();
         }
-        ec.addFunctionHandler(new FunctionExtensions.TodayFunc("today", customDate));
-        ec.addFunctionHandler(new FunctionExtensions.TodayFunc("now", customDate));
+
+        ec.addFunctionHandler(new FunctionExtensions.TodayFunc("today", hardCodedDate));
+        ec.addFunctionHandler(new FunctionExtensions.TodayFunc("now", hardCodedDate));
+
         return ec;
     }
 
@@ -116,7 +123,6 @@ public class XFormEnvironment {
         hardCodedDate = DateUtils.parseDate(dateString);
         System.out.println(hardCodedDate);
     }
-
 
     public void commitStep() {
         if(recording) {
