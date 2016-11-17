f484737ad70b06ed5ad03965c1faad739e4cb55c
Will Pride <wpride@dimagi.com>
2016-11-17 18:17:28 -0500

    revert harness

--- javarosa/util/schema-gen/src/org/javarosa/engine/Harness.java
+++ javarosa/util/schema-gen/src/org/javarosa/engine/Harness.java
@@ -11,7 +11,11 @@ import org.javarosa.engine.xml.serializer.MockupSerializer;
 import org.javarosa.xml.util.InvalidStructureException;
 import org.xmlpull.v1.XmlPullParserException;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * @author ctsims
