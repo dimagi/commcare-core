d09337aedfcc2b93c02b0d02ce755720f2f075de
Will Pride <wpride@dimagi.com>
2016-11-17 18:59:44 -0500

    Fix session state again

--- backend/src/org/commcare/session/CommCareSession.java
+++ backend/src/org/commcare/session/CommCareSession.java
@@ -481,6 +481,7 @@ public class CommCareSession {
 
         for (StackFrameStep step : frame.getSteps()) {
             if (SessionFrame.STATE_DATUM_VAL.equals(step.getType()) ||
+                    SessionFrame.STATE_DATUM_COMPUTED.equals(step.getType()) ||
                     SessionFrame.STATE_UNKNOWN.equals(step.getType()) &&
                             (guessUnknownType(step).equals(SessionFrame.STATE_DATUM_COMPUTED)
                             || guessUnknownType(step).equals(SessionFrame.STATE_DATUM_VAL))) {
