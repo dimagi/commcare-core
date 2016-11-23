2a00565f43f738262890edfefce9db2d901e5754
Will Pride <wpride@dimagi.com>
2016-11-23 09:51:01 -0500

    add format-date-for-calendar docs

--- javarosa/src/main/java/org/javarosa/xpath/expr/XPathFormatDateForCalendarFunc.java
+++ javarosa/src/main/java/org/javarosa/xpath/expr/XPathFormatDateForCalendarFunc.java
@@ -50,6 +50,11 @@ public class XPathFormatDateForCalendarFunc extends XPathFuncExpr {
 
     @Override
     public String getDocumentation() {
-        return "?";
+        return getDocHeader()
+                + "Behavior: Converts a Gregorian formatted date to a different calendar format. \n"
+                + "Return: a String of the input date formatted for the specified calendar. \n"
+                + "Arguments: The date to be converted and the format to convert to "
+                    + "(currently 'ethiopian' or 'nepali'). \n"
+                + "Example: format-date-for-calendar('19070-01-1', 'nepali')";
     }
 }
