23b10c1a21da13712d02b7a3cf474957cb1b3b1f
Phillip Mates <pmates@dimagi.com>
2016-12-06 12:29:46 -0200

    wip: loading form in JMH code

--- src/jmh/java/org/commcare/HelloWorld.java
+++ src/jmh/java/org/commcare/HelloWorld.java
@@ -3,13 +3,15 @@ package org.commcare;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Group;
 import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 import java.util.Random;
+
+import org.javarosa.core.test.FormParseInit;
+import org.javarosa.core.model.test.FormDefTest;
 
 import static org.openjdk.jmh.annotations.Scope.Benchmark;
 
@@ -26,60 +28,11 @@ public class HelloWorld {
         }
     }
 
-    @Benchmark
-    @Group("variableaccess")
-    public void messWithSameVariable(Blackhole bh){
-        int x = 5;
-        int y = 3;
-        x *= 5;
-        x *= 3;
-        bh.consume(x);
-        bh.consume(y);
-    }
-    @Benchmark
-    @Group("variableaccess")
-    public void messWithDifferentVariables(Blackhole bh){
-        int x = 5;
-        int y = 3;
-        x *= 5;
-        y *= 3;
-        bh.consume(x);
-        bh.consume(y);
-    }
-    @Benchmark
-    @Group("search")
-    public int linearSearch() {
-        final int needle = Numbers.rnd.nextInt(Numbers.sortedNumbers.length);
-        for (int x = 0; x < Numbers.sortedNumbers.length; x++) {
-            final int currentValue = Numbers.sortedNumbers[x];
-            if (currentValue == needle) {
-                return x;
-            }
-            if (currentValue > needle) {
-                return -1;
-            }
-
-        }
-        return -1;
-    }
-
     @Benchmark()
     @Group("search")
-    public int binarySearch() {
-        final int needle = Numbers.rnd.nextInt(Numbers.sortedNumbers.length);
-        int lo = 0;
-        int hi = Numbers.sortedNumbers.length - 1;
-        boolean found = false;
-        while (lo <= hi) {
-            final int mid = lo + (hi - lo) / 2;
-            final int currentValue = Numbers.sortedNumbers[mid];
-            if (needle < currentValue) hi = mid - 1;
-            else if (needle > currentValue) lo = mid + 1;
-            else {
-                return mid;
-            }
-        }
-        return -1;
+    public void recForm() {
+        FormParseInit fpi = new FormParseInit("/format_date_tests.xml");
+        FormDefTest.initFormEntry(fpi);
     }
 
     // Execute using:
