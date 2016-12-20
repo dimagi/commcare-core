859e023bea3491b7c03b1ee26c4534e13a2bef31
Phillip Mates <pmates@dimagi.com>
2016-12-20 16:35:57 -0200

    WIP: add storaged backed version of fixture for flat fixtures

---
+++ src/main/java/org/commcare/cases/instance/FixtureChildElement.java
@@ -0,0 +1,94 @@
+package org.commcare.cases.instance;
+
+import org.commcare.cases.model.StorageBackedModel;
+import org.javarosa.core.model.data.StringData;
+import org.javarosa.core.model.instance.TreeElement;
+import org.javarosa.core.model.instance.TreeReference;
+import org.javarosa.xpath.expr.XPathPathExpr;
+
+import java.util.Hashtable;
+
+/**
+ * @author Phillip Mates (pmates@dimagi.com)
+ */
+
+public class FixtureChildElement extends StorageBackedChildElement<StorageBackedModel> {
+    private String name;
+    private TreeElement empty;
+    private Hashtable<XPathPathExpr, Hashtable<String, TreeElement[]>> childAttributeHintMap = null;
+
+    protected FixtureChildElement(StorageInstanceTreeElement<StorageBackedModel, ?> parent, int mult, int recordId, String name) {
+        super(parent, mult, recordId, null, null);
+        this.name = name;
+    }
+
+    /*
+     * Template constructor (For elements that need to create reference nodesets but never look up values)
+     */
+    private FixtureChildElement(StorageInstanceTreeElement<StorageBackedModel, ?> parent) {
+        super(parent, TreeReference.INDEX_TEMPLATE, TreeReference.INDEX_TEMPLATE, null, null);
+
+        empty = new TreeElement(name);
+        empty.setMult(this.mult);
+        empty.setAttribute(null, nameId, "");
+
+        StorageBackedModel modelTemplate = parent.getModelTemplate();
+
+        Hashtable<String, String> attributes = modelTemplate.getAttributes();
+        for (String key : attributes.keySet()) {
+            empty.setAttribute(null, key, "");
+        }
+
+        Hashtable<String, String> elements = modelTemplate.getElements();
+        for (String key : elements.keySet()) {
+            TreeElement scratch = new TreeElement(key);
+            scratch.setAnswer(null);
+            empty.addChild(scratch);
+        }
+    }
+
+    @Override
+    protected TreeElement cache() {
+        if (recordId == TreeReference.INDEX_TEMPLATE) {
+            return empty;
+        }
+
+        synchronized (parent.treeCache) {
+            TreeElement element = parent.treeCache.retrieve(recordId);
+            if (element != null) {
+                return element;
+            }
+
+            TreeElement cacheBuilder = new TreeElement(name);
+
+            StorageBackedModel model = parent.getElement(recordId);
+            entityId = model.getEntityId();
+            cacheBuilder.setMult(mult);
+
+            Hashtable<String, String> attributes = model.getAttributes();
+            for (String key : attributes.keySet()) {
+                empty.setAttribute(null, key, attributes.get(key));
+            }
+
+            Hashtable<String, String> elements = model.getElements();
+            for (String key : elements.keySet()) {
+                TreeElement scratch = new TreeElement(key);
+                String data = elements.get(key);
+                // TODO PLM: do we want smarter type dispatch?
+                scratch.setAnswer(new StringData(data == null ? "" : data));
+                empty.addChild(scratch);
+            }
+
+            cacheBuilder.setParent(this.parent);
+
+            parent.treeCache.register(recordId, cacheBuilder);
+
+            return cacheBuilder;
+        }
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/org/commcare/cases/model/StorageBackedModel.java
@@ -0,0 +1,72 @@
+package org.commcare.cases.model;
+
+import org.javarosa.core.services.storage.IMetaData;
+import org.javarosa.core.services.storage.Persistable;
+import org.javarosa.core.util.externalizable.DeserializationException;
+import org.javarosa.core.util.externalizable.ExtUtil;
+import org.javarosa.core.util.externalizable.ExtWrapMap;
+import org.javarosa.core.util.externalizable.PrototypeFactory;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Hashtable;
+
+/**
+ * @author Phillip Mates (pmates@dimagi.com)
+ */
+public class StorageBackedModel implements Persistable, IMetaData {
+
+    private Hashtable<String, String> attributes = new Hashtable<>();
+    private Hashtable<String, String> elements = new Hashtable<>();
+    protected int recordId;
+    protected String entityId;
+
+    public Hashtable<String, String> getAttributes() {
+        return attributes;
+    }
+
+    public Hashtable<String, String> getElements() {
+        return elements;
+    }
+
+    @Override
+    public String[] getMetaDataFields() {
+        return new String[0];
+    }
+
+    @Override
+    public Object getMetaData(String fieldName) {
+        return null;
+    }
+
+    @Override
+    public void setID(int id) {
+        this.recordId = id;
+    }
+
+    @Override
+    public int getID() {
+        return recordId;
+    }
+
+    public String getEntityId() {
+        return entityId;
+    }
+
+    @Override
+    public void readExternal(DataInputStream in, PrototypeFactory pf) throws IOException, DeserializationException {
+        recordId = ExtUtil.readInt(in);
+        entityId = ExtUtil.nullIfEmpty(ExtUtil.readString(in));
+        attributes = (Hashtable)ExtUtil.read(in, new ExtWrapMap(String.class, String.class), pf);
+        elements = (Hashtable)ExtUtil.read(in, new ExtWrapMap(String.class, String.class), pf);
+    }
+
+    @Override
+    public void writeExternal(DataOutputStream out) throws IOException {
+        ExtUtil.writeNumeric(out, recordId);
+        ExtUtil.writeString(out, ExtUtil.emptyIfNull(entityId));
+        ExtUtil.write(out, new ExtWrapMap(attributes));
+        ExtUtil.write(out, new ExtWrapMap(elements));
+    }
+}
