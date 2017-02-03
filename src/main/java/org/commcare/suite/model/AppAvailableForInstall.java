ac1bb9fe5f92f209f2363c394acdb41eb7b1bc2a
ALIZA STONE <amstone326@gmail.com>
2017-02-03 17:29:55 -0500

    request for available apps is working

---
+++ src/main/java/org/commcare/suite/model/AppAvailableForInstall.java
@@ -0,0 +1,29 @@
+package org.commcare.suite.model;
+
+/**
+ * Created by amstone326 on 2/3/17.
+ */
+
+public class AppAvailableForInstall {
+
+    private String domain;
+    private String appName;
+    private String appVersion;
+    private boolean isOnProd;
+    private String profileRef;
+    private String mediaProfileRef;
+
+    public AppAvailableForInstall(String domain, String appName, String appVersion,
+                                  boolean isOnProd, String profileRef, String mediaProfileRef) {
+        this.domain = domain;
+        this.appName = appName;
+        this.appVersion = appVersion;
+        this.isOnProd = isOnProd;
+        this.profileRef = profileRef;
+        this.mediaProfileRef = mediaProfileRef;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+}
