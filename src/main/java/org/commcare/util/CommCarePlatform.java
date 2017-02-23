6c52e9f2d2dd3355138f6bb465e071f71d3678cd
Will Pride <wpride@dimagi.com>
2017-02-23 14:27:23 +0200

    Missing static StorageManager reference

--- src/main/java/org/commcare/util/CommCarePlatform.java
+++ src/main/java/org/commcare/util/CommCarePlatform.java
@@ -69,7 +69,7 @@ public class CommCarePlatform {
 
     public Vector<Suite> getInstalledSuites() {
         Vector<Suite> installedSuites = new Vector<>();
-        IStorageUtilityIndexed utility = StorageManager.getStorage(Suite.STORAGE_KEY);
+        IStorageUtilityIndexed utility = StorageManager.instance().getStorage(Suite.STORAGE_KEY);
 
         IStorageIterator iterator = utility.iterate();
 
