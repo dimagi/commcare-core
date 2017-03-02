e96f12a6cd386d7c333c0490a4959a68082f8d6d
Will Pride <wpride@dimagi.com>
2017-03-02 11:10:09 +0200

    Allow selection value to be empty

--- src/api/java/org/commcare/api/json/PromptToJson.java
+++ src/api/java/org/commcare/api/json/PromptToJson.java
@@ -10,6 +10,7 @@ import org.javarosa.form.api.FormEntryCaption;
 import org.javarosa.form.api.FormEntryController;
 import org.javarosa.form.api.FormEntryModel;
 import org.javarosa.form.api.FormEntryPrompt;
+import org.javarosa.xpath.XPathTypeMismatchException;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.json.JSONArray;
