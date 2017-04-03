9643d92f73e8fdbd218234de73ef561a318d613a
Will Pride <wpride@dimagi.com>
2017-04-03 16:17:41 +0200

    Add polymorphic process() function

--- src/api/java/org/commcare/api/process/FormRecordProcessorHelper.java
+++ src/api/java/org/commcare/api/process/FormRecordProcessorHelper.java
@@ -2,6 +2,7 @@ package org.commcare.api.process;
 
 import org.commcare.core.interfaces.UserSandbox;
 import org.commcare.core.process.XmlFormRecordProcessor;
+import org.commcare.data.xml.TransactionParserFactory;
 import org.javarosa.xml.util.InvalidStructureException;
 import org.javarosa.xml.util.UnfullfilledRequirementsException;
 import org.xmlpull.v1.XmlPullParserException;
@@ -19,6 +20,14 @@ import java.io.InputStream;
  * Created by wpride1 on 8/20/15.
  */
 public class FormRecordProcessorHelper extends XmlFormRecordProcessor {
+
+    public static void processXML(TransactionParserFactory factory, String fileText) throws IOException, XmlPullParserException, UnfullfilledRequirementsException, InvalidStructureException {
+        InputStream stream = new ByteArrayInputStream(fileText.getBytes("UTF-8"));
+        FormRecordProcessorThread thread = new FormRecordProcessorThread(factory, stream);
+        thread.start();
+    }
+
+
     public static void processXML(UserSandbox sandbox, String fileText) throws IOException, XmlPullParserException, UnfullfilledRequirementsException, InvalidStructureException {
         InputStream stream = new ByteArrayInputStream(fileText.getBytes("UTF-8"));
         FormRecordProcessorThread thread = new FormRecordProcessorThread(sandbox, stream);
@@ -31,9 +40,13 @@ public class FormRecordProcessorHelper extends XmlFormRecordProcessor {
         thread.start();
     }
 
-    static class FormRecordProcessorThread extends Thread{
+    static class FormRecordProcessorThread extends Thread {
         public FormRecordProcessorThread(UserSandbox sandbox, InputStream stream) throws IOException, XmlPullParserException, UnfullfilledRequirementsException, InvalidStructureException {
             process(sandbox, stream);
+        }
+
+        public FormRecordProcessorThread(TransactionParserFactory factory, InputStream stream) throws IOException, XmlPullParserException, UnfullfilledRequirementsException, InvalidStructureException {
+            process(stream, factory);
         }
     }
 }
