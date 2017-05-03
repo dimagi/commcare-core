558f67be5ba3625a3693a04cb7d2f45cdc7a394d
Will Pride <wpride@dimagi.com>
2017-05-03 08:42:18 +0200

    more static

--- src/cli/java/org/commcare/util/screen/EntityScreen.java
+++ src/cli/java/org/commcare/util/screen/EntityScreen.java
@@ -113,7 +113,7 @@ public class EntityScreen extends CompoundScreenHost {
         return mCurrentScreen;
     }
 
-    public String getReturnValueFromSelection(TreeReference contextRef, EntityDatum needed, EvaluationContext context) {
+    public static String getReturnValueFromSelection(TreeReference contextRef, EntityDatum needed, EvaluationContext context) {
         // grab the session's (form) element reference, and load it.
         TreeReference elementRef =
                 XPathReference.getPathExpr(needed.getValue()).getReference();
