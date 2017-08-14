be11cacd714c94a760b4f526475d19089daac7e9
ALIZA STONE <amstone326@gmail.com>
2017-08-14 14:22:03 -0400

    apply InstanceNameAccumulatingAnalyzer to make evaluation context of display conditions more efficient

--- src/main/java/org/javarosa/xpath/analysis/XPathAccumulatingAnalyzer.java
+++ src/main/java/org/javarosa/xpath/analysis/XPathAccumulatingAnalyzer.java
@@ -1,9 +1,10 @@
 package org.javarosa.xpath.analysis;
 
 import org.javarosa.core.model.instance.TreeReference;
-import org.javarosa.xpath.expr.XPathPathExpr;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by amstone326 on 8/11/17.
@@ -14,14 +15,29 @@ public abstract class XPathAccumulatingAnalyzer<T> {
     protected List<T> accumulatedList;
 
     public void extractTargetValues(XPathAnalyzable analyzable) {
+        // so that the default behavior is to do nothing
     }
 
     public void extractTargetValues(TreeReference treeRef) {
+        // so that InstanceNameAccumulatingAnalyzer can override this
     }
 
-    public List<T> accumulate(XPathAnalyzable rootExpression) {
+    public List<T> accumulateAsList(XPathAnalyzable rootExpression) {
         rootExpression.applyAndPropagateAccumulatingAnalyzer(this);
         return accumulatedList;
+    }
+
+    public Set<T> accumulateAsSet(XPathAnalyzable rootExpression) {
+        rootExpression.applyAndPropagateAccumulatingAnalyzer(this);
+        return convertResultToSet();
+    }
+
+    private Set<T> convertResultToSet() {
+        Set<T> set = new HashSet<>();
+        for (T item : accumulatedList) {
+            set.add(item);
+        }
+        return set;
     }
 
 }
