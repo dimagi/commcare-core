d6cb102d4fae769208f5c52e086f29372f92f81e
ALIZA STONE <amstone326@gmail.com>
2017-12-28 15:29:47 -0500

    log this exception so we can track it

--- src/main/java/org/javarosa/core/model/condition/EvaluationContext.java
+++ src/main/java/org/javarosa/core/model/condition/EvaluationContext.java
@@ -13,6 +13,7 @@ import org.javarosa.core.model.trace.BulkEvaluationTrace;
 import org.javarosa.core.model.trace.EvaluationTrace;
 import org.javarosa.core.model.trace.EvaluationTraceReporter;
 import org.javarosa.core.model.utils.CacheHost;
+import org.javarosa.core.services.Logger;
 import org.javarosa.xpath.IExprDataType;
 import org.javarosa.xpath.XPathLazyNodeset;
 import org.javarosa.xpath.XPathMissingInstanceException;
@@ -520,7 +521,9 @@ public class EvaluationContext {
             instance = this.getInstance(qualifiedRef.getInstanceName());
         }
         if (instance == null) {
-            throw new XPathMissingInstanceException(qualifiedRef);
+            XPathMissingInstanceException e = new XPathMissingInstanceException(qualifiedRef);
+            Logger.exception(e.getMessage(), e);
+            throw e;
         }
         return instance.resolveReference(qualifiedRef, this);
     }
