3b42c096be8465deb98e5b0350c2a2d8308682b0
ALIZA STONE <amstone326@gmail.com>
2018-01-05 14:38:42 -0500

    change comment

--- src/main/java/org/javarosa/core/model/data/helper/ValueResolutionContext.java
+++ src/main/java/org/javarosa/core/model/data/helper/ValueResolutionContext.java
@@ -2,7 +2,7 @@ package org.javarosa.core.model.data.helper;
 
 /**
  * Provides contextualizing information needed to properly resolve or write out a final value (such as in operations
- * like serialization or casting)
+ * like serialization and casting)
  */
 public class ValueResolutionContext {
 
