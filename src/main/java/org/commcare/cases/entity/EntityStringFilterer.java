d57e9f28da17c96e69d48a5ad3d814240ce07281
Will Pride <wpride@dimagi.com>
2018-02-16 14:57:21 -0500

    Update EntityStringFilterer to remove unused variables/patterns from Anroid

--- src/main/java/org/commcare/cases/entity/EntityStringFilterer.java
+++ src/main/java/org/commcare/cases/entity/EntityStringFilterer.java
@@ -1,40 +1,34 @@
 package org.commcare.cases.entity;
 
-import org.commcare.cases.util.StringUtils;
 import org.commcare.modern.util.Pair;
 import org.javarosa.core.model.instance.TreeReference;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 
 /**
  * Filter entity list via all string-representable entity fields
  */
 public class EntityStringFilterer {
-    private final boolean isFilterEmpty;
     private final String[] searchTerms;
     private final ArrayList<Pair<Integer, Integer>> matchScores = new ArrayList<>();
-    private final boolean isAsyncMode;
-    private final boolean isFuzzySearchEnabled;
 
     private final NodeEntityFactory nodeFactory;
-    //private final EntityListAdapter adapter;
     protected final List<Entity<TreeReference>> matchList;
     protected final List<Entity<TreeReference>> fullEntityList;
 
 
     public EntityStringFilterer(String[] searchTerms,
-                                boolean isAsyncMode, boolean isFuzzySearchEnabled,
                                 NodeEntityFactory nodeFactory,
                                 List<Entity<TreeReference>> fullEntityList) {
         this.matchList = new ArrayList<>();
         this.nodeFactory = nodeFactory;
         this.fullEntityList = fullEntityList;
-        this.isAsyncMode = isAsyncMode;
-        this.isFuzzySearchEnabled = isFuzzySearchEnabled;
-        this.isFilterEmpty = searchTerms == null || searchTerms.length == 0;
         this.searchTerms = searchTerms;
-        if (isFilterEmpty) {
+        if (searchTerms == null || searchTerms.length == 0) {
             matchList.addAll(fullEntityList);
         }
     }
@@ -50,8 +44,6 @@ public class EntityStringFilterer {
         }
 
         Locale currentLocale = Locale.getDefault();
-        //It's a bit sketchy here, because this DB lock will prevent
-        //anything else from processing
         for (int index = 0; index < fullEntityList.size(); ++index) {
             Entity<TreeReference> e = fullEntityList.get(index);
             boolean add = false;
@@ -64,39 +56,15 @@ public class EntityStringFilterer {
                     if (!"".equals(field) && field.toLowerCase(currentLocale).contains(filter.toLowerCase())) {
                         add = true;
                         continue filter;
-                    } else if (isFuzzySearchEnabled) {
-                        // We possibly now want to test for edit distance for
-                        // fuzzy matching
-                        for (String fieldChunk : e.getSortFieldPieces(i)) {
-                            Pair<Boolean, Integer> match = StringUtils.fuzzyMatch(filter, fieldChunk);
-                            if (match.first) {
-                                add = true;
-                                score += match.second;
-                                continue filter;
-                            }
-                        }
                     }
-                }
-                if (!add) {
-                    break;
                 }
             }
             if (add) {
                 matchScores.add(Pair.create(index, score));
             }
         }
-        if (isAsyncMode) {
-            Collections.sort(matchScores, new Comparator<Pair<Integer, Integer>>() {
-                @Override
-                public int compare(Pair<Integer, Integer> lhs, Pair<Integer, Integer> rhs) {
-                    return lhs.second - rhs.second;
-                }
-            });
-        }
 
-        for (Pair<Integer, Integer> match : matchScores) {
-            matchList.add(fullEntityList.get(match.first));
-        }
+        matchList.addAll(matchScores.stream().map(match -> fullEntityList.get(match.first)).collect(Collectors.toList()));
         return matchList;
     }
 }
