ddcd99545db648ba4ed32f52295db1a42640e0e6
ALIZA STONE <amstone326@gmail.com>
2018-04-23 12:27:19 -0400

    linting

--- src/translate/java/org/javarosa/engine/xml/TreeElementParser.java
+++ src/translate/java/org/javarosa/engine/xml/TreeElementParser.java
@@ -29,34 +29,34 @@ public class TreeElementParser extends ElementParser<TreeElement> {
         int depth = parser.getDepth();
         TreeElement element = new TreeElement(parser.getName(), multiplicity);
         element.setInstanceName(instanceId);
-        for(int i = 0 ; i < parser.getAttributeCount(); ++i) {
+        for (int i = 0 ; i < parser.getAttributeCount(); ++i) {
             element.setAttribute(parser.getAttributeNamespace(i), parser.getAttributeName(i), parser.getAttributeValue(i));
         }
 
         Hashtable<String, Integer> multiplicities = new Hashtable<>();
         //NOTE: We never expect this to be the exit condition
-        while(parser.getDepth() >= depth) {
-        switch(this.nextNonWhitespace()) {
-            case KXmlParser.START_TAG:
-                String name = parser.getName();
-                int val;
-                if(multiplicities.containsKey(name)) {
-                    val = multiplicities.get(name) +1;
-                } else {
-                    val = 0;
-                }
-                multiplicities.put(name,  val);
+        while (parser.getDepth() >= depth) {
+            switch (this.nextNonWhitespace()) {
+                case KXmlParser.START_TAG:
+                    String name = parser.getName();
+                    int val;
+                    if (multiplicities.containsKey(name)) {
+                        val = multiplicities.get(name) +1;
+                    } else {
+                        val = 0;
+                    }
+                    multiplicities.put(name,  val);
 
-                TreeElement kid = new TreeElementParser(parser, val, instanceId).parse();
-                element.addChild(kid);
-                break;
-            case KXmlParser.END_TAG:
-                return element;
-            case KXmlParser.TEXT:
-                element.setValue(new UncastData(parser.getText().trim()));
-                break;
-            default:
-                throw new InvalidStructureException("Exception while trying to parse an XML Tree, got something other than tags and text", parser);
+                    TreeElement kid = new TreeElementParser(parser, val, instanceId).parse();
+                    element.addChild(kid);
+                    break;
+                case KXmlParser.END_TAG:
+                    return element;
+                case KXmlParser.TEXT:
+                    element.setValue(new UncastData(parser.getText().trim()));
+                    break;
+                default:
+                    throw new InvalidStructureException("Exception while trying to parse an XML Tree, got something other than tags and text", parser);
             }
         }
 
