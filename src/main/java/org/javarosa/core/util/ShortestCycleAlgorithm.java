0c33143b398e61b40f58cc2fb7fc73404668d98b
Will Pride <wpride@dimagi.com>
2018-06-20 10:32:55 -0400

    Don't repeat edges or nodes, don't re-walk walks

--- src/main/java/org/javarosa/core/util/ShortestCycleAlgorithm.java
+++ src/main/java/org/javarosa/core/util/ShortestCycleAlgorithm.java
@@ -2,8 +2,11 @@ package org.javarosa.core.util;
 
 import org.javarosa.core.model.instance.TreeReference;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Vector;
 
 /**
@@ -18,9 +21,11 @@ import java.util.Vector;
 public class ShortestCycleAlgorithm {
 
     Vector<TreeReference[]> edges;
-    ArrayList<String> nodes = new ArrayList<>();
-    Hashtable<String, ArrayList<String>> childrenMap = new OrderedHashtable<>();
+    ArrayList<String> nodes = new ArrayList<String>();
+    Hashtable<String, ArrayList<String>> childrenMap = new OrderedHashtable<String, ArrayList<String>>();
     ArrayList<String> shortestCycle = null;
+    Hashtable<String, ArrayList<String>> reachableMap = new OrderedHashtable<String, ArrayList<String>>();
+    ArrayList<String> walked = new ArrayList<String>();
 
     public ShortestCycleAlgorithm(Vector<TreeReference[]> edges) {
         this.edges = edges;
@@ -28,7 +33,9 @@ public class ShortestCycleAlgorithm {
             String parentKey = references[0].toString();
             String childKey = references[1].toString();
             addChild(parentKey, childKey);
-            nodes.add(parentKey);
+            if (!nodes.contains(parentKey)) {
+                nodes.add(parentKey);
+            }
         }
 
         for (String node: nodes) {
@@ -44,26 +51,59 @@ public class ShortestCycleAlgorithm {
             childrenMap.put(parentKey, new ArrayList<String>());
         }
         ArrayList<String> childList = childrenMap.get(parentKey);
-        childList.add(childKey);
+        if (!childList.contains(childKey)) {
+            childList.add(childKey);
+        }
+    }
+
+    // Add the new node to the set of reachable nodes for all already-visited nodes
+    private void addReachbleToVisited(List<String> visited, String reachable) {
+        for (String visit: visited) {
+            addReachable(visit, reachable);
+        }
+    }
+
+    private void addReachable(String parent, String reachable) {
+        if (!reachableMap.containsKey(parent)) {
+            reachableMap.put(parent, new ArrayList<String>());
+        }
+        ArrayList<String> reachableList = reachableMap.get(parent);
+        if (!reachableList.contains(reachable)) {
+            reachableList.add(reachable);
+        }
     }
 
     private ArrayList<String> depthFirstSearch(String startNode, String currentNode, ArrayList<String> visited) {
+        addReachbleToVisited(visited, currentNode);
         if (visited.contains(currentNode)) {
             if (startNode.equals(currentNode)) {
                 return visited;
             }
             return null;
         }
+
         visited.add(currentNode);
         ArrayList<String> children = childrenMap.get(currentNode);
         if (children != null) {
             for (String child : children) {
+
+                // If we have already walked this node, get the set of reachable nodes from that walk
+                // If that set does not contain any of the visited nodes in the current walk
+                // Then this child cannot contain a cycle
+                if (walked.contains(child)) {
+                    ArrayList<String> reachables = reachableMap.get(child);
+                    if (reachables == null || !reachables.contains(visited)) {
+                        continue;
+                    }
+                }
+
                 ArrayList<String> shortestPath = depthFirstSearch(startNode, child, visited);
                 if (shortestPath != null) {
                     return shortestPath;
                 }
             }
         }
+        walked.add(currentNode);
         visited.remove(currentNode);
         return null;
     }
@@ -77,11 +117,11 @@ public class ShortestCycleAlgorithm {
         for (int i = 0; i < shortestCycle.size(); i++) {
             stringBuilder.append(shortestCycle.get(i));
             if (i == shortestCycle.size() - 1) {
-                stringBuilder.append(" references ");
+                stringBuilder.append(" is referenced by ");
                 stringBuilder.append(shortestCycle.get(0));
                 stringBuilder.append(".");
             } else {
-                stringBuilder.append( " references " );
+                stringBuilder.append( " is referenced by " );
                 stringBuilder.append(shortestCycle.get(i + 1));
                 stringBuilder.append( ", \n" );
             }
