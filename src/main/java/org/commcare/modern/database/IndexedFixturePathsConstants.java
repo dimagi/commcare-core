5007fd7cc9750ba0c79cce24f5114b1fb92986da
Shubham Goyal <shubham1g5@gmail.com>
2019-02-22 12:42:45 +0530

    Adds last_sync col to Indexed fixture index table

--- src/main/java/org/commcare/modern/database/IndexedFixturePathsConstants.java
+++ src/main/java/org/commcare/modern/database/IndexedFixturePathsConstants.java
@@ -8,21 +8,24 @@ public class IndexedFixturePathsConstants {
     public final static String INDEXED_FIXTURE_PATHS_COL_NAME = "name";
     public final static String INDEXED_FIXTURE_PATHS_COL_BASE = "base";
     public final static String INDEXED_FIXTURE_PATHS_COL_CHILD = "child";
+    public final static String INDEXED_FIXTURE_PATHS_COL_LAST_SYNC = "last_sync";
     public final static String INDEXED_FIXTURE_PATHS_TABLE_STMT =
+            "CREATE TABLE IF NOT EXISTS " +
+                    IndexedFixturePathsConstants.INDEXED_FIXTURE_PATHS_TABLE +
+                    " (" + IndexedFixturePathsConstants.INDEXED_FIXTURE_PATHS_COL_NAME + " UNIQUE" +
+                    ", " + IndexedFixturePathsConstants.INDEXED_FIXTURE_PATHS_COL_BASE +
+                    ", " + IndexedFixturePathsConstants.INDEXED_FIXTURE_PATHS_COL_CHILD +
+                    ", " + IndexedFixturePathsConstants.INDEXED_FIXTURE_PATHS_COL_LAST_SYNC + ");";
+
+    public final static String INDEXED_FIXTURE_INDEXING_STMT =
+            DatabaseIndexingUtils.indexOnTableCommand("fixture_name_index",
+                    IndexedFixturePathsConstants.INDEXED_FIXTURE_PATHS_TABLE, IndexedFixturePathsConstants.INDEXED_FIXTURE_PATHS_COL_NAME);
+
+    public final static String INDEXED_FIXTURE_PATHS_TABLE_STMT_V15 =
             "CREATE TABLE IF NOT EXISTS " +
                     IndexedFixturePathsConstants.INDEXED_FIXTURE_PATHS_TABLE +
                     " (" + IndexedFixturePathsConstants.INDEXED_FIXTURE_PATHS_COL_NAME + " UNIQUE" +
                     ", " + IndexedFixturePathsConstants.INDEXED_FIXTURE_PATHS_COL_BASE +
                     ", " + IndexedFixturePathsConstants.INDEXED_FIXTURE_PATHS_COL_CHILD + ");";
 
-    public final static String INDEXED_FIXTURE_PATHS_TABLE_SELECT_STMT =
-            "SELECT " +
-                    IndexedFixturePathsConstants.INDEXED_FIXTURE_PATHS_COL_BASE + ", " +
-                    IndexedFixturePathsConstants.INDEXED_FIXTURE_PATHS_COL_CHILD  +
-                    " FROM " + IndexedFixturePathsConstants.INDEXED_FIXTURE_PATHS_TABLE +
-                    " WHERE " + IndexedFixturePathsConstants.INDEXED_FIXTURE_PATHS_COL_NAME + " = ?;";
-
-    public final static String INDEXED_FIXTURE_INDEXING_STMT =
-            DatabaseIndexingUtils.indexOnTableCommand("fixture_name_index",
-                    IndexedFixturePathsConstants.INDEXED_FIXTURE_PATHS_TABLE, IndexedFixturePathsConstants.INDEXED_FIXTURE_PATHS_COL_NAME);
 }
