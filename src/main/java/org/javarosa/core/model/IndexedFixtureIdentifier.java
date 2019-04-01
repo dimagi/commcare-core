a73d86801085e41d20c640bb6f54449e616642b7
Shubham Goyal <shubham1g5@gmail.com>
2019-04-01 19:00:01 +0530

    Cleanup

--- src/main/java/org/javarosa/core/model/IndexedFixtureIdentifier.java
+++ src/main/java/org/javarosa/core/model/IndexedFixtureIdentifier.java
@@ -12,51 +12,29 @@ import javax.annotation.Nullable;
  */
 
 public class IndexedFixtureIdentifier {
-    private String fixtureName;
     private String fixtureBase;
     private String fixtureChild;
 
     @Nullable
     private TreeElement rootAttributes;
 
-    public IndexedFixtureIdentifier(String fixtureName, String fixtureBase, String fixtureChild, @Nullable TreeElement rootAttributes) {
-        this.fixtureName = fixtureName;
+    public IndexedFixtureIdentifier(String fixtureBase, String fixtureChild, @Nullable TreeElement rootAttributes) {
         this.fixtureBase = fixtureBase;
         this.fixtureChild = fixtureChild;
         this.rootAttributes = rootAttributes;
-    }
-
-    public String getFixtureName() {
-        return fixtureName;
-    }
-
-    public void setFixtureName(String name) {
-        this.fixtureName = name;
     }
 
     public String getFixtureBase() {
         return fixtureBase;
     }
 
-    public void setFixtureBase(String fixtureBase) {
-        this.fixtureBase = fixtureBase;
-    }
-
     public String getFixtureChild() {
         return fixtureChild;
-    }
-
-    public void setChild(String child) {
-        this.fixtureChild = child;
     }
 
     @Nullable
     public TreeElement getRootAttributes() {
         return rootAttributes;
-    }
-
-    public void setRootAttributes(@Nullable TreeElement rootAttributes) {
-        this.rootAttributes = rootAttributes;
     }
 }
 
