1fdfd3b10ff461c07151854bd59dfe65a4d45f72
Clayton Sims <csims@dimagi.com>
2020-07-07 16:16:15 -0400

    Tests for edge cases

--- src/test/java/org/javarosa/xform/parse/SendParseTests.java
+++ src/test/java/org/javarosa/xform/parse/SendParseTests.java
@@ -1,6 +1,7 @@
 package org.javarosa.xform.parse;
 
 import org.javarosa.core.model.SubmissionProfile;
+import org.javarosa.core.services.locale.Localizer;
 import org.javarosa.core.test.FormParseInit;
 import org.javarosa.model.xform.XPathReference;
 import org.junit.Assert;
@@ -25,6 +26,13 @@ public class SendParseTests {
     @Test(expected = XFormParseException.class)
     public void missingSubmission() {
         new FormParseInit("/send_action/missing_submission.xml");
+    }
+
+    @Test
+    public void testUnicode() {
+        FormParseInit fpi = new FormParseInit("/xform_tests/itext_encoding.xml");
+        Localizer l = fpi.getFormDef().getLocalizer();
+        Assert.assertEquals("\uD83E\uDDD2", l.getText("four_byte_emoji","en"));
     }
 
 }
