d0d84aece38480cc2634584187bf8811b9e02cac
Kevin Nguyen <knguyen@dimagi.com>
2020-09-23 09:22:29 -0400

    deleted extra function

--- src/main/java/org/javarosa/xpath/parser/ast/ASTNodeFunctionCall.java
+++ src/main/java/org/javarosa/xpath/parser/ast/ASTNodeFunctionCall.java
@@ -61,8 +61,6 @@ public class ASTNodeFunctionCall extends ASTNode {
                 return new XPathBooleanFromStringFunc(args);
             case "format-date":
                 return new XPathFormatDateFunc(args);
-            case "format-date-with-tz":
-                return new XPathFormatDateWithTimezoneFunc(args);
             case "selected":
                 // fall-through to is-selected on purpose
             case "is-selected":
