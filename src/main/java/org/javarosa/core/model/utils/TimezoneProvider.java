d92c21f418e13c0767192854d026b20851734499
Kevin Nguyen <knguyen@dimagi.com>
2020-10-08 18:22:43 -0400

    provide timezone option

--- src/main/java/org/javarosa/core/model/utils/TimezoneProvider.java
+++ src/main/java/org/javarosa/core/model/utils/TimezoneProvider.java
@@ -1,5 +1,7 @@
 package org.javarosa.core.model.utils;
 
+import java.util.TimeZone;
+
 /**
  * Created by amstone326 on 1/5/18.
  */
@@ -7,6 +9,10 @@ public class TimezoneProvider {
 
     public int getTimezoneOffsetMillis() {
         return -1;
+    }
+
+    public TimeZone getTimezone() {
+        return null;
     }
 
 }
