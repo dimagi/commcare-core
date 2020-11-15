a71b72d6c9b1977f29b683de30bd8df4612080f8
Jennifer Schweers <orange.jenny@gmail.com>
2020-11-15 15:37:38 -0500

    Added auto_launch option for actions

--- src/cli/java/org/commcare/util/screen/EntityScreen.java
+++ src/cli/java/org/commcare/util/screen/EntityScreen.java
@@ -74,6 +74,10 @@ public class EntityScreen extends CompoundScreenHost {
     }
 
     public void init(SessionWrapper session) throws CommCareSessionException {
+        init(session, false);
+    }
+
+    public void init(SessionWrapper session, boolean allowAutoLaunch) throws CommCareSessionException {
         if (initialized) {
             return;
         }
@@ -98,6 +102,19 @@ public class EntityScreen extends CompoundScreenHost {
         }
 
         evalContext = mSession.getEvaluationContext();
+
+        for (Action action : mShortDetail.getCustomActions(evalContext)) {
+            if (action.isAutoLaunching()) {
+                // Supply an empty case list so we can "select" from it later using getEntityFromID
+                mCurrentScreen = new EntityListSubscreen(mShortDetail, new Vector<TreeReference>(), evalContext, handleCaseIndex);
+                full = false;
+                if (allowAutoLaunch) {
+                    this.setPendingAction(action);
+                    this.updateSession(session);
+                    return;
+                }
+            }
+        }
 
         references = expandEntityReferenceSet(evalContext);
 
@@ -188,7 +205,11 @@ public class EntityScreen extends CompoundScreenHost {
     }
 
     public void setHighlightedEntity(String id) throws CommCareSessionException {
-        this.mCurrentSelection = referenceMap.get(id);
+        if (referenceMap == null) {
+            this.mCurrentSelection = mNeededDatum.getEntityFromID(evalContext, id);
+        } else {
+            this.mCurrentSelection = referenceMap.get(id);
+        }
         if (this.mCurrentSelection == null) {
             throw new CommCareSessionException("EntityScreen " + this.toString() + " could not select case " + id + "." +
                     " If this error persists please report a bug to CommCareHQ.");
