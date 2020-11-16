b4a728409fd22d4706f12e316710070b871d8f1e
Cal Ellowitz <calellowitz@gmail.com>
2020-11-16 15:45:12 -0500

    add new indices to bulk parser

--- src/main/java/org/commcare/xml/bulk/BulkProcessingCaseXmlParser.java
+++ src/main/java/org/commcare/xml/bulk/BulkProcessingCaseXmlParser.java
@@ -213,6 +213,12 @@ public abstract class BulkProcessingCaseXmlParser extends BulkElementParser<Case
                 case "external_id":
                     caseForBlock.setExternalId(value);
                     break;
+                case "patient_type":
+                    caseForBlock.setPatientType(value);
+                    break;
+                case "current_status":
+                    caseForBlock.setCurrentStatus(value);
+                    break;
                 default:
                     caseForBlock.setProperty(key, value);
                     break;
