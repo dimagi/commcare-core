5c7170c42508fcf38ef424c9c5637e94437f3f7f
Jennifer Schweers <orange.jenny@gmail.com>
2020-12-09 20:51:27 -0500

    Removed old setHighlightedEntity logic, no longer used

--- src/cli/java/org/commcare/util/screen/EntityScreen.java
+++ src/cli/java/org/commcare/util/screen/EntityScreen.java
@@ -206,11 +206,7 @@ public class EntityScreen extends CompoundScreenHost {
     }
 
     public void setHighlightedEntity(String id) throws CommCareSessionException {
-        if (referenceMap == null) {
-            this.mCurrentSelection = mNeededDatum.getEntityFromID(evalContext, id);
-        } else {
-            this.mCurrentSelection = referenceMap.get(id);
-        }
+        this.mCurrentSelection = referenceMap.get(id);
         if (this.mCurrentSelection == null) {
             throw new CommCareSessionException("EntityScreen " + this.toString() + " could not select case " + id + "." +
                     " If this error persists please report a bug to CommCareHQ.");
