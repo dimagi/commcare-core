ab84c942d6ec563a9e76ea304601d8572c36ce2e
Cal Ellowitz <calellowitz@gmail.com>
2021-01-14 16:14:21 -0500

    add ref tags for choice population

--- src/main/java/org/javarosa/core/model/FormDef.java
+++ src/main/java/org/javarosa/core/model/FormDef.java
@@ -1358,6 +1358,9 @@ public class FormDef implements IFormElement, IMetaData,
 
     @Trace
     public void populateDynamicChoices(ItemsetBinding itemset, TreeReference curQRef){
+        final Span span = GlobalTracer.get().activeSpan();
+        span.setTag("itemset", itemset.nodesetRef.toString());
+        span.setTag("treeReference", curQRef.toString());
         ItemSetUtils.populateDynamicChoices(itemset, curQRef, exprEvalContext, getMainInstance(), mProfilingEnabled);
     }
 
