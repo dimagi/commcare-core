32f29517eb66d742af2881c8e75ed1d10b1e7afc
Cal Ellowitz <calellowitz@gmail.com>
2021-04-20 11:32:12 -0400

    pull getNeededDatum out of loop

--- src/cli/java/org/commcare/util/screen/EntityScreen.java
+++ src/cli/java/org/commcare/util/screen/EntityScreen.java
@@ -109,8 +109,9 @@ public class EntityScreen extends CompoundScreenHost {
 
         if (full || references.size() == 1) {
             referenceMap = new Hashtable<>();
+            EntityDatum needed = (EntityDatum) session.getNeededDatum();
             for(TreeReference reference: references) {
-                referenceMap.put(getReturnValueFromSelection(reference, (EntityDatum) session.getNeededDatum(), evalContext), reference);
+                referenceMap.put(getReturnValueFromSelection(reference, needed, evalContext), reference);
             }
 
             // for now override 'here()' with the coords of Sao Paulo, eventually allow dynamic setting
@@ -324,8 +325,9 @@ public class EntityScreen extends CompoundScreenHost {
         if (referenceMap != null) {
             return referenceMap.containsKey(stepValue);
         }
+        EntityDatum needed = (EntityDatum) session.getNeededDatum();
         for (TreeReference ref: references) {
-            String id = getReturnValueFromSelection(ref,(EntityDatum) session.getNeededDatum(), evalContext);
+            String id = getReturnValueFromSelection(ref, needed, evalContext);
             if (id.equals(stepValue)) {
                 return true;
             }
