bd7be5f9bdc74be17856972fadf729b16aa8e997
Simon Kelly <skelly@dimagi.com>
2022-03-16 16:56:09 +0200

    utilities for building instance trees

---
+++ src/main/java/org/commcare/data/xml/TreeBuilder.java
@@ -0,0 +1,50 @@
+package org.commcare.data.xml;
+
+import org.javarosa.core.model.data.UncastData;
+import org.javarosa.core.model.instance.TreeElement;
+
+import java.util.Hashtable;
+import java.util.List;
+
+/**
+ * Class to build a TreeElement tree for use with DataInstance classes
+ */
+public class TreeBuilder {
+
+    /**
+     * Build a TreeElement populated with children
+     */
+    public static TreeElement buildTree(String instanceId, String rootElementName, List<SimpleNode> nodes) {
+        TreeElement root = new TreeElement(rootElementName, 0);
+        root.setInstanceName(instanceId);
+        root.setAttribute(null, "id", instanceId);
+        addChildren(instanceId, root, nodes);
+        return root;
+    }
+
+    private static void addChildren(String instanceId, TreeElement parent, List<SimpleNode> nodes) {
+        Hashtable<String, Integer> multiplicities = new Hashtable<>();
+        for (SimpleNode node : nodes) {
+            int val;
+            String name = node.getName();
+            if (multiplicities.containsKey(name)) {
+                val = multiplicities.get(name) + 1;
+            } else {
+                val = 0;
+            }
+            multiplicities.put(name, val);
+
+            TreeElement element = new TreeElement(name, val);
+            element.setInstanceName(instanceId);
+            node.getAttributes().forEach((attributeName, value) -> {
+                element.setAttribute(null, attributeName, value);
+            });
+            if (node.getText() != null) {
+                element.setValue(new UncastData(node.getText()));
+            } else if (node.getChildren() != null) {
+                addChildren(instanceId, element, node.getChildren());
+            }
+            parent.addChild(element);
+        }
+    }
+}
---
+++ src/test/java/org/commcare/data/xml/VirtualInstancesTest.java
@@ -0,0 +1,38 @@
+package org.commcare.data.xml;
+
+import com.google.common.collect.ImmutableMap;
+import org.javarosa.core.model.instance.ExternalDataInstance;
+import org.javarosa.core.model.instance.TreeElement;
+import org.javarosa.xml.util.InvalidStructureException;
+import org.javarosa.xml.util.UnfullfilledRequirementsException;
+import org.junit.Test;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
+
+public class VirtualInstancesTest {
+    @Test
+    public void testBuildSearchInputRoot() throws UnfullfilledRequirementsException, XmlPullParserException, InvalidStructureException, IOException {
+        TreeElement root = VirtualInstances.buildSearchInputRoot(ImmutableMap.of(
+            "key0", "val0",
+            "key1", "val1",
+            "key2", "val2"
+        ));
+        String expectedXml = String.join(
+            "",
+            "<input id=\"search-input\">",
+                "<field name=\"key0\">val0</field>",
+                "<field name=\"key1\">val1</field>",
+                "<field name=\"key2\">val2</field>",
+            "</input>"
+        );
+        TreeElement expected = ExternalDataInstance.parseExternalTree(
+                new ByteArrayInputStream(expectedXml.getBytes(StandardCharsets.UTF_8)), VirtualInstances.SEARCH_INSTANCE_ID
+        );
+        assertEquals(expected, root);
+    }
+}
