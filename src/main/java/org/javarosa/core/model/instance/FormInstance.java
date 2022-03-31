78b3e9e23e97aef4ed89542213a6ae9a135b6be3
Graham Herceg <g.a.herceg@gmail.com>
2022-03-31 07:37:50 -0700

    Revert "Revert "Add formVersion in readExternal and writeExternal FormInstance methods""

--- src/main/java/org/javarosa/core/model/instance/FormInstance.java
+++ src/main/java/org/javarosa/core/model/instance/FormInstance.java
@@ -176,6 +176,8 @@ public class FormInstance extends DataInstance<TreeElement> implements Persistab
         super.readExternal(in, pf);
         schema = (String)ExtUtil.read(in, new ExtWrapNullable(String.class), pf);
         dateSaved = (Date)ExtUtil.read(in, new ExtWrapNullable(Date.class), pf);
+        uiVersion = (String)ExtUtil.read(in, new ExtWrapNullable(String.class), pf);
+        formVersion = (String)ExtUtil.read(in, new ExtWrapNullable(String.class), pf);
 
         namespaces = (Hashtable<String, String>)ExtUtil.read(in, new ExtWrapMap(String.class, String.class), pf);
         setRoot((TreeElement)ExtUtil.read(in, TreeElement.class, pf));
@@ -186,6 +188,8 @@ public class FormInstance extends DataInstance<TreeElement> implements Persistab
         super.writeExternal(out);
         ExtUtil.write(out, new ExtWrapNullable(schema));
         ExtUtil.write(out, new ExtWrapNullable(dateSaved));
+        ExtUtil.write(out, new ExtWrapNullable(uiVersion));
+        ExtUtil.write(out, new ExtWrapNullable(formVersion));
         ExtUtil.write(out, new ExtWrapMap(namespaces));
 
         ExtUtil.write(out, getRoot());
