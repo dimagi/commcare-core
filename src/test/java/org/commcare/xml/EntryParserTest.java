a69f09a6b0600fe6e1674bbe42a9e348f0bcbf1a
Simon Kelly <skelly@dimagi.com>
2022-04-29 13:46:18 +0200

    rename test

--- src/test/java/org/commcare/xml/EntryParserTest.java
+++ src/test/java/org/commcare/xml/EntryParserTest.java
@@ -1,9 +1,12 @@
 package org.commcare.xml;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import com.google.common.collect.Multimap;
 
+import org.commcare.suite.model.Entry;
+import org.commcare.suite.model.FormEntry;
 import org.commcare.suite.model.PostRequest;
 import org.commcare.suite.model.QueryData;
 import org.commcare.suite.model.RemoteRequestEntry;
@@ -12,22 +15,23 @@ import org.javarosa.test_utils.ReflectionUtils;
 import org.javarosa.xml.util.InvalidStructureException;
 import org.javarosa.xml.util.UnfullfilledRequirementsException;
 import org.junit.Test;
+import org.kxml2.io.KXmlParser;
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * Tests for {@link EntryParser} when parsing {@code <remote-request>} elements.
  */
-public class RemoteRequestEntryParserTest {
+public class EntryParserTest {
 
     @Test
-    public void testParse() throws IOException, UnfullfilledRequirementsException, InvalidStructureException,
+    public void testParseRemoteRequest() throws IOException, UnfullfilledRequirementsException, InvalidStructureException,
             XmlPullParserException, NoSuchFieldException, IllegalAccessException {
-        String query = "<remote-request>\n"
+        String xml = "<remote-request>\n"
                 + "  <post url=\"https://www.fake.com/claim_patient/\">\n"
                 + "    <data key=\"case_id\" ref=\"instance('session')/session/case_id\"/>\n"
                 + "  </post>\n"
@@ -37,7 +41,7 @@ public class RemoteRequestEntryParserTest {
                 + "    </display>\n"
                 + "  </command>\n"
                 + "</remote-request>";
-        PostRequest post = getRemoteRequestPost(query);
+        PostRequest post = getRemoteRequestPost(xml);
         List<QueryData> params = (List<QueryData>) ReflectionUtils.getField(post, "params");
         assertEquals(1, params.size());
         assertEquals("case_id", params.get(0).getKey());
