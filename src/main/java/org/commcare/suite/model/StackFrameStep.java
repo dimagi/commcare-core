0137b93b59440e4619aee679aa1fa532773ab9ed
Simon Kelly <skelly@dimagi.com>
2022-05-26 12:33:58 +0200

    use Objects.requireNonNull

--- src/main/java/org/commcare/suite/model/StackFrameStep.java
+++ src/main/java/org/commcare/suite/model/StackFrameStep.java
@@ -29,6 +29,7 @@ import java.util.Collection;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -101,10 +102,8 @@ public class StackFrameStep implements Externalizable {
     }
 
     public void addDataInstanceSource(ExternalDataInstanceSource source) {
-        if (source == null) {
-            throw new RuntimeException(String.format(
-                    "Unable to add null instance data source to stack frame step '%s'", getId()));
-        }
+        Objects.requireNonNull(source, String.format(
+                "Unable to add null instance data source to stack frame step '%s'", getId()));
         String instanceId = source.getInstanceId();
         if (dataInstanceSources.containsKey(instanceId)) {
             throw new RuntimeException(String.format(
