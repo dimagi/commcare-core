be2179ce035f4b07cc7588b142d2d7b794c63027
Jonathan Tang <Jonahtang738@gmail.com>
2023-01-24 11:49:24 -0800

    sticklr

--- src/main/java/org/commcare/suite/model/Detail.java
+++ src/main/java/org/commcare/suite/model/Detail.java
@@ -186,8 +186,7 @@ public class Detail implements Externalizable {
     /**
      * Returns the text displayed when case list results contain no items.
      * 
-     * @return The text to be displayed to users if case list
-     * has no items.
+     * @return returns Text for empty case list.
      */
     @Nullable
     public Text getNoItemsText() {
