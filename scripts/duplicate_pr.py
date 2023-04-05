8aa3697f01c77eef43a8dbdb2d96ddc568af1f67
Jonathan Tang <Jonahtang738@gmail.com>
2023-04-04 17:08:16 -0700

    hardcode new commit sha

--- scripts/duplicate_pr.py
+++ scripts/duplicate_pr.py
@@ -77,7 +77,7 @@ def merge_base_commit(branch1: str, branch2:str):
     git = get_git()
     print("in merge_base, before git call.")
     # base_commit = git("merge-base", branch1, branch2)
-    base_commit = "cb7c1e485d3bd8ce2c795cb819824b8328ee71b2"
+    base_commit = "793bad7e2b3448da5ed6f6f3900e04568a91e6ea"
     print("base commit is", base_commit)
     return str(base_commit.replace("\n", ""))
 
