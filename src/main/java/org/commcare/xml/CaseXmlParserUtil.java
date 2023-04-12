518c046835dccd5a4324634c9ecd4d878ac35823
Shubham Goyal <shubham1g5@gmail.com>
2023-04-11 22:12:09 -0300

    move constants to a util class

---
+++ src/main/java/org/commcare/xml/CaseXmlParserUtil.java
@@ -0,0 +1,21 @@
+package org.commcare.xml;
+
+public class CaseXmlParserUtil {
+
+    public static final String CASE_PROPERTY_CASE_ID = "case_id";
+    public static final String CASE_PROPERTY_CASE_TYPE = "case_type";
+    public static final String CASE_PROPERTY_OWNER_ID = "owner_id";
+    public static final String CASE_PROPERTY_STATUS = "status";
+    public static final String CASE_PROPERTY_CASE_NAME = "case_name";
+    public static final String CASE_PROPERTY_LAST_MODIFIED = "last_modified";
+    public static final String CASE_PROPERTY_DATE_OPENED = "date_opened";
+    public static final String CASE_PROPERTY_EXTERNAL_ID = "external_id";
+    public static final String CASE_PROPERTY_CATEGORY = "category";
+    public static final String CASE_PROPERTY_STATE = "state";
+    public static final String CASE_PROPERTY_INDEX = "index";
+    public static final String ATTACHMENT_FROM_LOCAL = "local";
+    public static final String ATTACHMENT_FROM_REMOTE = "remote";
+    public static final String ATTACHMENT_FROM_INLINE = "inline";
+    public static final String CASE_XML_NAMESPACE = "http://commcarehq.org/case/transaction/v2";
+
+}
