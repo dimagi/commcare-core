15242661186f520cee8bc8c5d290fb8d5bb884be
Shubham Goyal <shubham1g5@gmail.com>
2023-07-17 16:02:33 -0500

    Parse instance argument for endpoint

--- src/main/java/org/commcare/xml/EndpointParser.java
+++ src/main/java/org/commcare/xml/EndpointParser.java
@@ -1,6 +1,7 @@
 package org.commcare.xml;
 
 import org.commcare.suite.model.Endpoint;
+import org.commcare.suite.model.EndpointArgument;
 import org.commcare.suite.model.StackOperation;
 import org.javarosa.xml.ElementParser;
 import org.javarosa.xml.util.InvalidStructureException;
@@ -17,6 +18,9 @@ public class EndpointParser extends ElementParser<Endpoint> {
 
     static final String NAME_ENDPOINT = "endpoint";
     private static final String NAME_ARGUMENT = "argument";
+    private static final String ATTR_ARGUMENT_ID = "id";
+    private static final String ATTR_ARGUMENT_INSTANCE_ID = "instance-id";
+    private static final String ATTR_ARGUMENT_INSTANCE_SRC = "instance-src";
 
 
     public EndpointParser(KXmlParser parser) {
@@ -31,16 +35,23 @@ public class EndpointParser extends ElementParser<Endpoint> {
         }
 
         Vector<StackOperation> stackOperations = new Vector<>();
-        Vector<String> arguments = new Vector<>();
+        Vector<EndpointArgument> arguments = new Vector<>();
 
         while (nextTagInBlock(NAME_ENDPOINT)) {
             String tagName = parser.getName().toLowerCase();
             if (tagName.contentEquals(NAME_ARGUMENT)) {
-                String argumentID = parser.getAttributeValue(null, "id");
+                String argumentID = parser.getAttributeValue(null, ATTR_ARGUMENT_ID);
                 if (argumentID == null || argumentID.isEmpty()) {
                     throw new InvalidStructureException("argument must define a non empty id", parser);
                 }
-                arguments.add(argumentID);
+                String argInstanceId = parser.getAttributeValue(null, ATTR_ARGUMENT_INSTANCE_ID);
+                String argInstanceSrc = parser.getAttributeValue(null, ATTR_ARGUMENT_INSTANCE_SRC);
+
+                if (argInstanceId != null && argInstanceSrc == null) {
+                    throw new InvalidStructureException(
+                            "argument containing a non-null instance-id must define an instance-src", parser);
+                }
+                arguments.add(new EndpointArgument(argumentID, argInstanceId, argInstanceSrc));
             } else if (tagName.contentEquals(NAME_STACK)) {
                 StackOpParser sop = new StackOpParser(parser);
                 while (this.nextTagInBlock(NAME_STACK)) {
