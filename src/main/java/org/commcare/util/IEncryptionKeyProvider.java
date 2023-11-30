276c050e425f820d2454f642e801a51b03c2ae75
Ahmad Vazirna <avazirna@gmail.com>
2023-11-30 15:49:52 +0200

    Add EncryptionKeyProvider service interface

---
+++ src/main/java/org/commcare/util/IEncryptionKeyProvider.java
@@ -0,0 +1,30 @@
+package org.commcare.util;
+
+import java.io.IOException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableEntryException;
+import java.security.cert.CertificateException;
+
+/**
+ * Service interface for Encryption Key providers for KeyStores
+ *
+ * @author avazirna
+ */
+
+public interface IEncryptionKeyProvider {
+
+    EncryptionKeyAndTransformation retrieveKeyFromKeyStore(String keyAlias,
+                                                           EncryptionUtils.CryptographicOperation operation)
+            throws KeyStoreException, UnrecoverableEntryException, NoSuchAlgorithmException, CertificateException, IOException;
+
+    void generateCryptographicKeyInKeyStore(String keyAlias);
+
+    boolean isKeyStoreAvailable();
+
+    String getTransformationString(String algorithm);
+
+    String getAESKeyAlgorithmRepresentation();
+
+    String getRSAKeyAlgorithmRepresentation();
+}
