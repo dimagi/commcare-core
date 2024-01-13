81dc60e92eaae648bfdab5546c08031d021f59e9
Jonathan Tang <Jonahtang738@gmail.com>
2024-01-12 20:01:28 -0800

    feat: parses for searchOnClear in suite

--- src/main/java/org/commcare/xml/SessionDatumParser.java
+++ src/main/java/org/commcare/xml/SessionDatumParser.java
@@ -129,6 +129,7 @@ public class SessionDatumParser extends CommCareElementParser<SessionDatum> {
 
         boolean defaultSearch = "true".equals(parser.getAttributeValue(null, "default_search"));
         boolean dynamicSearch = "true".equals(parser.getAttributeValue(null, "dynamic_search"));
+        boolean searchOnClear = "true".equals(parser.getAttributeValue(null, "search_on_clear"));
         Text title = null;
         Text description = null;
         Hashtable<String, QueryGroup> groupPrompts = new Hashtable<>();
@@ -152,6 +153,7 @@ public class SessionDatumParser extends CommCareElementParser<SessionDatum> {
             }
         }
         return new RemoteQueryDatum(queryUrl, queryResultStorageInstance, hiddenQueryValues,
-            userQueryPrompts, useCaseTemplate, defaultSearch, dynamicSearch, title, description, groupPrompts);
+            userQueryPrompts, useCaseTemplate, defaultSearch, dynamicSearch, title, description, groupPrompts,
+            searchOnClear);
     }
 }
