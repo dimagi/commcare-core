cf44f92059a772dd28484591046fd2ac6dce3698
Minha <mlee@dimagi.com>
2024-10-10 18:30:17 +0200

    Refactor initialization functions for DataInstance (and dervied classes) to avoid putting so many null values into argument lists

--- src/main/java/org/javarosa/model/xform/DataModelSerializer.java
+++ src/main/java/org/javarosa/model/xform/DataModelSerializer.java
@@ -33,7 +33,7 @@ public class DataModelSerializer {
     }
 
     public void serialize(ExternalDataInstance instance, TreeReference base) throws IOException {
-        DataInstance specializedInstance = instance.initialize(factory, instance.getInstanceId(),null);
+        DataInstance specializedInstance = instance.initialize(factory, instance.getInstanceId());
         serialize(specializedInstance, base);
     }
 
