fb16f46f978063e3e5415b3c47f21367cb0c17aa
Minha <mlee@dimagi.com>
2024-10-10 14:18:24 +0200

    Fix tests

--- src/test/java/org/commcare/test/utilities/TestInstanceInitializer.java
+++ src/test/java/org/commcare/test/utilities/TestInstanceInitializer.java
@@ -31,7 +31,7 @@ public class TestInstanceInitializer extends InstanceInitializationFactory {
     }
 
     @Override
-    public InstanceRoot generateRoot(ExternalDataInstance instance) {
+    public InstanceRoot generateRoot(ExternalDataInstance instance, String locale) {
         String ref = instance.getReference();
         if (ref.contains(CaseInstanceTreeElement.MODEL_NAME)) {
             CaseInstanceTreeElement root = new CaseInstanceTreeElement(instance.getBase(), sandbox.getCaseStorage());
