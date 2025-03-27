38ac31382fce7e4abc1acf60b4591ee9c866f3c0
Minha <mlee@dimagi.com>
2025-03-27 16:31:13 -0400

    Add Document control type

--- src/main/java/org/javarosa/core/model/Constants.java
+++ src/main/java/org/javarosa/core/model/Constants.java
@@ -79,6 +79,7 @@ public class Constants {
     public static final int CONTROL_LABEL = 11;
     public static final int CONTROL_AUDIO_CAPTURE = 12;
     public static final int CONTROL_VIDEO_CAPTURE = 13;
+    public static final int CONTROL_DOCUMENT_UPLOAD = 14;
 
     /**
      * constants for xform tags
