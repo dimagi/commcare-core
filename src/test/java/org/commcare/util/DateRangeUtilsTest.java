2940eb1fb93c2afb8ba0a2713b3a49522c5f8cf7
David Viggiano <dave.viggiano@gmail.com>
2025-05-01 09:46:11 -0400

    Added back commented out code (accidental commit)

--- src/test/java/org/commcare/util/DateRangeUtilsTest.java
+++ src/test/java/org/commcare/util/DateRangeUtilsTest.java
@@ -1,18 +1,18 @@
-//package org.commcare.util;
-//
-//import junit.framework.TestCase;
-//
-//import org.junit.Test;
-//
-//import java.text.ParseException;
-//
-//public class DateRangeUtilsTest extends TestCase {
-//
-//    @Test
-//    public void testDateConversion() throws ParseException {
-//        String dateRange = "2020-02-15 to 2021-03-18";
-//        String formattedDateRange = DateRangeUtils.formatDateRangeAnswer(dateRange);
-//        assertEquals("__range__2020-02-15__2021-03-18", formattedDateRange);
-//        assertEquals(dateRange, DateRangeUtils.getHumanReadableDateRange(formattedDateRange));
-//    }
-//}
+package org.commcare.util;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import java.text.ParseException;
+
+public class DateRangeUtilsTest extends TestCase {
+
+    @Test
+    public void testDateConversion() throws ParseException {
+        String dateRange = "2020-02-15 to 2021-03-18";
+        String formattedDateRange = DateRangeUtils.formatDateRangeAnswer(dateRange);
+        assertEquals("__range__2020-02-15__2021-03-18", formattedDateRange);
+        assertEquals(dateRange, DateRangeUtils.getHumanReadableDateRange(formattedDateRange));
+    }
+}
