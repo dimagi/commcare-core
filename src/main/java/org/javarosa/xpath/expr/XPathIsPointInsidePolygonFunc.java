a7b37c735d7d14f458e13842d23d2c94f7a1f236
parthmittal <pmittal@dimagi-associate.com>
2025-05-06 19:21:15 +0530

    -updated the functions for polygon-point and point-inside

--- src/main/java/org/javarosa/xpath/expr/XPathIsPointInsidePolygonFunc.java
+++ src/main/java/org/javarosa/xpath/expr/XPathIsPointInsidePolygonFunc.java
@@ -7,54 +7,53 @@ import org.javarosa.core.model.instance.DataInstance;
 import org.javarosa.core.model.utils.PolygonUtils;
 import org.javarosa.xpath.XPathTypeMismatchException;
 import org.javarosa.xpath.parser.XPathSyntaxException;
-import java.util.ArrayList;
-import java.util.List;
+import org.locationtech.jts.geom.Polygon;
 
-public class XPathPointInsidePolygon extends XPathFuncExpr{
+import java.util.Arrays;
+
+public class XPathIsPointInsidePolygonFunc extends XPathFuncExpr {
     public static final String NAME = "inside-polygon";
     private static final int EXPECTED_ARG_COUNT = 2;
+
     /**
      * Returns true if the geopoint is inside the polygon, in meters, given objects to unpack.
      * Ignores altitude and accuracy.
      * Note that the arguments can be strings.
      * Returns false if one of the arguments is null or the empty string.
      */
-    public XPathPointInsidePolygon() {
+    public XPathIsPointInsidePolygonFunc() {
         name = NAME;
         expectedArgCount = EXPECTED_ARG_COUNT;
     }
 
-    public XPathPointInsidePolygon(XPathExpression[] args) throws XPathSyntaxException {
+    public XPathIsPointInsidePolygonFunc(XPathExpression[] args) throws XPathSyntaxException {
         super(NAME, args, EXPECTED_ARG_COUNT, true);
     }
 
 
     @Override
     protected Object evalBody(DataInstance model, EvaluationContext evalContext, Object[] evaluatedArgs) {
-        return boundaryDistance(evaluatedArgs[0], evaluatedArgs[1]);
+        return isPointWithinBoundary(evaluatedArgs[0], evaluatedArgs[1]);
     }
 
-    public static boolean boundaryDistance(Object from, Object to) {
+    public static boolean isPointWithinBoundary(Object from, Object to) {
         String unpackedFrom = (String)FunctionUtils.unpack(from);
         String unpackedTo = (String)FunctionUtils.unpack(to);
         if (unpackedFrom == null || "".equals(unpackedFrom) || unpackedTo == null || "".equals(unpackedTo)) {
             return false;
         }
         try {
-            String[] coordinates=unpackedFrom.split(" ");
-            List<Double> polygonList = new ArrayList<Double>();
-
-            for (String coordinate : coordinates) {
-                polygonList.add(Double.parseDouble(coordinate));
-            }
+            String[] coordinates = unpackedFrom.split(" ");
+            Polygon polygon = PolygonUtils.createValidatedPolygon(Arrays.asList(coordinates));
             // Casting and uncasting seems strange but is consistent with the codebase
-            GeoPointData castedTo = new GeoPointData().cast(new UncastData(unpackedTo));
-            boolean isInside= PolygonUtils.isPointInsidePolygon(polygonList,new double[]{castedTo.getLatitude(), castedTo.getLongitude()});
+            GeoPointData pointData = new GeoPointData().cast(new UncastData(unpackedTo));
 
-            return isInside;
+            return PolygonUtils.isPointInsideOrOnPolygon(polygon, pointData);
         } catch (NumberFormatException e) {
             throw new XPathTypeMismatchException("point-in-boundary() function requires arguments containing " +
                     "numeric values only, but received arguments: " + unpackedFrom + " and " + unpackedTo);
+        } catch (IllegalArgumentException e) {
+            throw new XPathTypeMismatchException(e.getMessage());
         }
     }
 }
