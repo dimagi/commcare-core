273342c59479114f831bea0185ee5c2d82025a4b
Shubham Goyal <sgoyal@dimagi.com>
2025-11-01 10:12:35 +0530

    Apply suggestion from @shubham1g5

--- src/cli/java/org/commcare/util/cli/CliMain.java
+++ src/cli/java/org/commcare/util/cli/CliMain.java
@@ -64,7 +64,7 @@ public class CliMain {
         try {
             cliCommand.handle();
         } catch (Exception e) {
-            System.err.println("\nValidation failed with error:");
+            System.err.println("\Command failed with error:");
             e.printStackTrace();
             System.exit(-1);
         }
