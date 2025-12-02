1d3c02c9070a4c5c6591d649eea98cfd2395880b
Shubham Goyal <shubham1g5@gmail.com>
2025-12-02 22:41:40 +0530

    adds argument validations for auth classes

--- src/main/java/org/commcare/core/network/AuthInfo.java
+++ src/main/java/org/commcare/core/network/AuthInfo.java
@@ -1,5 +1,7 @@
 package org.commcare.core.network;
 
+import org.commcare.cases.util.StringUtils;
+
 /**
  * Created by amstone326 on 5/8/18.
  */
@@ -22,6 +24,12 @@ public abstract class AuthInfo {
         }
 
         public ProvidedAuth(String username, String password, boolean wrapDomain) {
+            if (StringUtils.isEmpty(username)) {
+                throw new IllegalArgumentException("ProvidedAuth requires a non-empty username");
+            }
+            if (StringUtils.isEmpty(password)) {
+                throw new IllegalArgumentException("ProvidedAuth requires a non-empty password");
+            }
             this.username = username;
             this.password = password;
             this.wrapDomain = wrapDomain;
@@ -35,6 +43,9 @@ public abstract class AuthInfo {
 
     public static class TokenAuth extends AuthInfo {
         public TokenAuth(String token) {
+            if (StringUtils.isEmpty(token)) {
+                throw new IllegalArgumentException("TokenAuth requires a non-empty token");
+            }
             bearerToken = token;
         }
     }
